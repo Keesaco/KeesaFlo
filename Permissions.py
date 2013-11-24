91de303ce07f418483927d1d2f3d63224be01310
Jack McCrea <contact@jpm.im>
2013-11-24 01:57:10 +0000

    Add methods to permissions and PermissionsObject classes

--- Permissions.py
+++ Permissions.py
@@ -3,6 +3,13 @@ class Permissions:
 	write = False
 	fullControl = False
 	
-	def __init__(self):
-		pass
+	def __init__(self, read = False, write = False, fullControl = False):
+		self.setPermissions(read, write, fullControl)
+
+	def setPermissions(self, read = False, write = False, fullControl = False):
+		self.read = read
+		self.write = write
+		self.fullControl = fullControl
+
+
 		
