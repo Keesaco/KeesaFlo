56853290be8438466886ad35b8f34e64ddff115a
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-11-25 18:44:35 +0000

    Constructor for APIAnalysis

--- shared/python/API/APIAnalysis.py
+++ shared/python/API/APIAnalysis.py
@@ -8,3 +8,22 @@ from PALAnalysis import *
 ## APIAnalysis
 # Second tier API for analysis - utilises PAL for platform specific analysis
 class APIAnalysis:
+
+	## constructor
+	# \author swhitehouse@keesaco.com of Keesaco
+	def __init__(self):
+		pass
+		
+	## add_file creates a new file and optionally opens it in a given mode
+	# \param data_ref - reference to the data to be analysis
+	# \param plugin_ref - reference to the plugin to be used for analysis
+	# \param user_id - the id of the user starting the analysis
+	# \param priority - a value determining the priority of the specific analysis
+	# \On fail, return false. On success, returns the analysis_id for the specific analysis request
+	# \author swhitehouse@keesaco.com of Keesaco
+	# \author rmurley@keesaco.com of Keesaco
+	def queue_analysis(	data_ref,
+						plugin_ref,
+						user_id,
+						priority	):
+		pass
