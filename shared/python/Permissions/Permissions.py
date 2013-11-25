c6624b94f2d07797c6e040369c2fcc92df59666f
Jack McCrea <contact@jpm.im>
2013-11-25 01:16:56 +0000

    For read and write true if full control is granted in Permissions, document Permissions class

--- shared/python/Permissions/Permissions.py
+++ shared/python/Permissions/Permissions.py
@@ -1,15 +1,42 @@
+## \file shared/python/permissions/Permissions.py
+# \brief Contains the Permissions class for storing a set of permissions one entity has to a resource
+# \author jmccrea@keesaco.com of Keesaco
+
 class Permissions:
+	## read access
 	read = False
+	## write access
 	write = False
-	fullControl = False
+	## full control
+	full_control = False
 	
-	def __init__(self, read = False, write = False, fullControl = False):
+	## \brief Constructs a Permissions instance with given permissions
+	# \param self - intance reference
+	# \param read - (= False) [Boolean] read access
+	# \param write - (= False) [Boolean] write access
+	# \param full_control - (= False) [Boolean] full control
+	# \return A Permissions instance with the given permissions
+	# \note if full_control is true, read and write will also be forced true
+	# \author jmccrea@keesaco.com of Keesaco
+	def __init__(self, read = False, write = False, full_control = False):
 		self.setPermissions(read, write, fullControl)
 
-	def setPermissions(self, read = False, write = False, fullControl = False):
-		self.read = read
-		self.write = write
-		self.fullControl = fullControl
+	## \brief sets the permissions to a given set of permissions
+	# \param self - intance reference
+	# \param read - (= False) [Boolean] set read access
+	# \param write - (= False) [Boolean] set write access
+	# \param full_control - (= False) [Boolean] full control
+	# \return none
+	# \note if full_control is True, read and write will also be forced True
+	# \author jmccrea@keesaco.com of Keesaco
+	def setPermissions(self, read = False, write = False, full_control = False):
+		self.full_control = full_control
+		if full_control:
+			self.read = True
+			self.write = True
+		else:
+			self.read = read
+			self.write = write
 
 
 		
