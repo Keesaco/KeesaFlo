c3ba3096029ab19d6955fce40ec3da2d8822fdcf
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-11-25 19:43:54 +0000

    Removed unnecessary function

--- shared/python/API/APIAnalysis.py
+++ shared/python/API/APIAnalysis.py
@@ -52,19 +52,4 @@ class APIAnalysis:
 									user_id,
 									priority	):
 		pass
-	
-	## change_user_analysis changes the owner of the current analysis instantiation
-	# \param analysis_id - the id of the analysis that is being passed on
-	# \param user_id - the id of the user requesting the change
-	# \param new_user_id - the id of the user the analysis instantiation is being transferred to.
-	# \return If failed, return false. Else if analysis user change is successful, return True.
-	# \note If the user_id does not match the one stored in the analysis_id then this function will fail. The new_user_id will be checked to make sure they have permissions for the data and the plugin with the function failing if they do not. Priority will also be modified to be in keeping with the new user's own priority.
-	# \author swhitehouse@keesaco.com of Keesaco
-	# \author rmurley@keesaco.com of Keesaco
-	def change_user_analysis(	analysis_id,
-								user_id,
-								new_user_id	):
-		pass
-		
-	
 
