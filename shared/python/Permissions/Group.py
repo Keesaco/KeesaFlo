8d59d6f8a67a23c0aba38e8e0590f2c861f70d21
Jack McCrea <contact@jpm.im>
2013-11-25 00:43:14 +0000

    document Group

--- shared/python/Permissions/Group.py
+++ shared/python/Permissions/Group.py
@@ -1,6 +1,15 @@
+## \file shared/python/permissions/Group.py
+# contains the group class
 
+## \class Group
+# \brief Group class for holding information about a usergroup
 class Group:
-	groupName = ""
+	group_name = "" ## Name of the user group
 	
+	## constructor - creates a Group instance with a given groupname
+	# \param self - instance reference
+	# \param name - (= None) [String] name of user group
+	# \return Group instance
+	# \author jmccrea@keesaco.com of Keesaco
 	def __init__(self, name = None):
-		self.groupName = name
+		self.group_name = name
