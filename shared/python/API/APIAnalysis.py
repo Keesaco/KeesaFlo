80b906058bcf6111656bb9f2a6cd1945d0b2b54a
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-11-25 17:41:28 +0000

    Added headers to both files
    Doxygen comments begun...

---
+++ shared/python/API/APIAnalysis.py
@@ -0,0 +1,10 @@
+## \file shared/python/API/APIAnalysis.py
+# \brief Defines the analysis API for analysing data. Built upon PALAnalysis for cross-platform support.
+# \author swhitehouse@keesaco.com of Keesaco
+# \author rmurley@keesaco.com of Keesaco
+
+from PALAnalysis import *
+
+## APIAnalysis
+# Second tier API for analysis - utilises PAL for platform specific analysis
+class APIAnalysis:
---
+++ shared/python/API/PALAnalysis.py
@@ -0,0 +1,6 @@
+## \file shared/python/API/PALAnalysis.py
+# \brief Contains the PALAnalysis class: Platform Abstraction Layer for analysis
+# \author swhitehouse@keesaco.com of Keesaco
+# \author rmurley@keesaco.com of Keesaco
+## API layer for 'low level' analysis
+class PALAnalysis:
