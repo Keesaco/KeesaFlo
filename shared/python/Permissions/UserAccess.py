c5b0777002d34c6142bd78d0cc718a6ab7888073
Jack McCrea <jmccrea@keesaco.com>
2013-11-25 14:43:42 +0000

    Update UserAccess constructor and document UserAccess class

--- shared/python/Permissions/UserAccess.py
+++ shared/python/Permissions/UserAccess.py
@@ -1,10 +1,23 @@
+## \file shared/python/permissions/UserAccess.py
+# \brief Contains the UserAccess class for user/permissions pairs
+# \author jmccrea@keesaco.com of Keesaco
+
 from User import *
 from Permissions import *
 
+## \brief A user and permissinos pair to represent the access permissions a user has for a resource
 class UserAccess:
+	## User instance - the user to define permissions for
 	user = User()
+	## Permissions instnace - the permissions the user has
 	permissions = Permissions()
 	
-	
-	def __init__(self):
-		pass
+	## \brief Constructs a UserAccess instance for given User and Permissions instances
+	# \param self - instance reference
+	# \param user - (= None) [User] the user which the permissions belong to
+	# \param permissions - (= None) [Permissions] permissions which the user has
+	# \return UserAccess instance for the given user and permissions
+	# \author jmccrea@keesaco.com of Keesaco
+	def __init__(self, user = None, permissions = None):
+		self.user = user
+		self.permissions = permissions
