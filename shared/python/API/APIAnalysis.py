6fdcc5601b320b96d4dd3e7ee9959cc5a7e71145
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-11-25 19:03:23 +0000

    Updated doxygen for API functions
    Also deleted unnecessary functions.

--- shared/python/API/APIAnalysis.py
+++ shared/python/API/APIAnalysis.py
@@ -14,7 +14,7 @@ class APIAnalysis:
 	def __init__(self):
 		pass
 
-	## add_file creates a new file and optionally opens it in a given mode
+	## queue_analysis requests that a new analysis be instantiated using a specified plugin and data-set
 	# \param data_ref - reference to the data to be analysis
 	# \param plugin_ref - reference to the plugin to be used for analysis
 	# \param user_id - the id of the user starting the analysis
@@ -27,19 +27,24 @@ class APIAnalysis:
 						user_id,
 						priority	):
 		pass
-		
-	def start_analysis(	analysis_id,
-						user_id	):
-		pass
 
-	def pause_analysis(	analysis_id,
-						user_id	):
-		pass
-		
+	## cancel_analysis requests that analysis instantiation is ended prematurely 
+	# \param analysis_id - the id of the analysis that needs to be ended
+	# \param user_id - the id of the user ending the analysis
+	# \return If the user_id does not match the one stored in the analysis_id, return false. Else if analysis termination is successful, return True.
+	# \author swhitehouse@keesaco.com of Keesaco
+	# \author rmurley@keesaco.com of Keesaco
 	def cancel_analysis(	analysis_id,
 							user_id	):
 		pass
-		
+
+	## change_priority_analysis requests that the priority of a currently running analysis instantiation is changed
+	# \param analysis_id - the id of the analysis that is having its priority ended
+	# \param user_id - the id of the user changing the priority
+	# \param priority - a value determining the priority that the analysis needs to be changed to
+	# \return If the user_id does not match the one stored in the analysis_id, return false. Else if analysis priority change is successful, return True.
+	# \author swhitehouse@keesaco.com of Keesaco
+	# \author rmurley@keesaco.com of Keesaco
 	def change_priority_analysis(	analysis_id,
 									user_id,
 									priority	):
