35cc1478ca25d929ccbf9b8ddaa92a2c71b303cc
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-11-27 16:00:55 +0000

    Updated PALAnalysis to include initialiser.

--- shared/python/API/PALAnalysis.py
+++ shared/python/API/PALAnalysis.py
@@ -1,7 +1,14 @@
 ## \file shared/python/API/PALAnalysis.py
-# \brief Contains the PALAnalysis class: Platform Abstraction Layer for analysis
+# \brief Contains the PALAnalysis class: Platform Abstraction Layer for plugin instantiation
 # \author swhitehouse@keesaco.com of Keesaco
 # \author rmurley@keesaco.com of Keesaco
 
-## \brief API layer for 'low level' analysis
+## \brief API layer for instantiation of plugins on the Google Cloud Platform
 class PALAnalysis:
+
+	## \brief Constructor for PALAnalysis.
+	# \param self - instance reference
+	# \return Returns the PALAnalysis instance.
+	# \author swhitehouse@keesaco.com of Keesaco
+	def __init__(	self	):
+		pass
