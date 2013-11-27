7d5512851186246085fa47724ec33ffca468918c
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-11-27 15:55:08 +0000

    Moved unneeded functions.
    Functions were moved from the APIAnalysis to the AnalysisScheduler so
    that the API is now more clear.

---
+++ analysis/manager/AnalysisManger.py
@@ -0,0 +1,18 @@
+## \file analysis/manager/AnalysisManager.py
+# \brief Defines the Analysis Manager which is responsible for instantiating plugins and providing information to the instantiations.
+# \author swhitehouse@keesaco.com of Keesaco
+
+from PALAnalysis import *
+
+## \brief Analysis Manager responsible for instantiating plugins and providing them with data.
+class AnalysisManager:
+
+	## Variable containing the current number of instantiated plugins
+	noOfInstantiations = 0
+	
+	## \brief Constructor for AnalysisManager.
+	# \param self - instance reference
+	# \return Returns the AnalysisManager instance.
+	# \author swhitehouse@keesaco.com of Keesaco
+	def __init__(	self	):
+		pass
