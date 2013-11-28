c7968b937e607bcb9f132132c6470a832b600ea2
Jack McCrea <jmccrea@keesaco.com>
2013-11-28 17:32:04 +0000

    Use variable arguments in call() and broadcast() methods

--- shared/python/ListenerCollection/ListenerCollection.py
+++ shared/python/ListenerCollection/ListenerCollection.py
@@ -12,19 +12,19 @@ class Listener:
 	# \author jmccrea@keesaco.com of Keesaco
 	def __init__(self, callback = None):
 		## callback for this listener
-		self.callback = callback			#set callback to passed callback
+		self.callback = callback				#set callback to passed callback
 		## mute flag to temporarily disable callbacks for this listener
-		self.__muted = False				#Start unmuted
+		self.__muted = False					#Start unmuted
 
 	## \brief Calls the callback method so long as the listener is not muted or otherwise prevented from being alerted
 	# \param self - instance reference
-	# \param arg - (= None) argument to call the callback with
+	# \param *args - argument list to pass to callback
 	# \return none
 	# \author jmccrea@keesaco.com of Keesaco
-	def call(self, arg = None):
-		if ( self.callback != None): 		#check a callback has been set
-			if (not self.__muted):			#do not call if this listener has been muted
-				self.callback(arg)			#call the listener's callback method
+	def call(self, *args, **kwargs):
+		if ( self.callback != None): 			#check a callback has been set
+			if (not self.__muted):				#do not call if this listener has been muted
+				self.callback(*args, **kwargs)	#call the listener's callback method
 
 	## \brief Mutes or unmutes the listener
 	# \param self - instance reference
@@ -32,21 +32,21 @@ class Listener:
 	# \return none
 	# \author jmccrea@keesaco.com of Keesaco
 	def mute(self, mute = True):
-		self.__muted = mute					#set the muted flag with the given argument
+		self.__muted = mute						#set the muted flag with the given argument
 
 	## \brief Unmutes the listener
 	# \param self - instance reference
 	# \return none
 	# \author jmccrea@keesaco.com of Keesaco
 	def unmute(self):
-		self.mute(False) 					#use the existing mute method in case more logic is added later
+		self.mute(False) 						#use the existing mute method in case more logic is added later
 
 	## \brief Gets the current mute status
 	# \param self - instance method
 	# \return [Bool] true if muted false otherwise
 	# \author jmccrea@keesaco.com Keesaco
 	def is_muted(self):
-		return self.__muted					#return muted flag
+		return self.__muted						#return muted flag
 
 ## \brief A simple collection of listeners
 class ListenerCollection:
@@ -56,7 +56,7 @@ class ListenerCollection:
 	# \return ListenerCollection object
 	# \author jmccrea@keesaco.com of Keesaco
 	def __init__(self):
-		self.listeners = []					#Initialise listeners with empty list
+		self.listeners = []						#Initialise listeners with empty list
 
 	## \brief Adds a listener to the listener collection
 	# \param self - instance reference
@@ -64,18 +64,18 @@ class ListenerCollection:
 	# \return position at which the new listener was added
 	# \author jmccrea@keesaco.com of Keesaco
 	def add_listener(self, listener):
-		new_position = len(self.listeners)	#get position of next inserted element (current length)
-		self.listeners.append(listener)		#add the new listener to the list
-		return new_position					#return previously stored position
+		new_position = len(self.listeners)		#get position of next inserted element (current length)
+		self.listeners.append(listener)			#add the new listener to the list
+		return new_position						#return previously stored position
 
 	## \brief alerts all callbacks (as long as they have not been muted)
 	# \param self - instance reference
-	# \param arg - argument to pass to callbacks
+	# \param *args - argument list to pass to callback
 	# \return none
 	# \author jmccrea@keesaco.com of Keesaco
-	def broadcast(self, arg):
-		for l in self.listeners:			#For each listener
-			l.call(arg)						#Call with given argument
+	def broadcast(self, *args, **kwargs):
+		for l in self.listeners:				#For each listener
+			l.call(*args, **kwargs)				#Call with given argument list
 
 # \brief A more versatile listener collection for multiple events and listeners
 class SubscriptionCollection:
@@ -109,8 +109,7 @@ class SubscriptionCollection:
 		if (listener_id < len(self.__listeners)) and (event_id < len(self.__events)):
 			self.__subscriptions[event_id][listener_id] = enable
 		else:
-			pass
-			#listener or event doesn't exist
+			pass							#listener or event doesn't exist
 
 	def unsubscribe(self, listener_id, event_id):
 		self.subscribe(listener_id, event_id, False)
