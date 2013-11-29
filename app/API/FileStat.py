e29753058051a26205ebeb6702b5e79c4d346e0c
Christian Wike <gitwike@gmail.com>
2013-11-29 16:10:59 +0000

    FileStat object implemented

--- app/API/FileStat.py
+++ app/API/FileStat.py
@@ -2,13 +2,19 @@
 
 class FileStat:
 	
-	def __init__(self):
-		pass
-		
-	self.filename
-	self.is_dir
-	self.st_size
-	self.etag
-	self.st_ctime
-	self.content_type
-	self.metadata
+	def __init__(	self,
+					filename,
+					st_size,
+					etag,
+					st_ctime,
+					content_type=None,
+					metadata=None,
+					is_dir=False)):
+	
+		self.filename = filename
+		self.is_dir	= is_dir
+		self.st_size = st_size
+		self.etag = etag
+		self.st_ctime = st_ctime
+		self.content_type = content_type
+		self.metadata = metadata
