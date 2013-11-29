1ad65fe2ab99656e2382240e1a89eb55f4c981ac
Christian Wike <gitwike@gmail.com>
2013-11-29 16:28:02 +0000

    Updated comments to standard formatting

--- app/API/PALDatastore.py
+++ app/API/PALDatastore.py
@@ -1,31 +1,26 @@
+###########################################################################
 ## \file app/API/PALDatastore.py
-# \brief Containts the PALDatastore class: Platform Abstraction Layer for data access
-# \author jmccrea@keesaco.com of Keesaco
-# \author cwike@keesaco.com of Keesaco
-
+## \brief Containts the PALDatastore package: Platform Abstraction Layer for data access
+## \author jmccrea@keesaco.com of Keesaco
+## \author cwike@keesaco.com of Keesaco
+###########################################################################
 import cloudstorage as gcs 
 
 ## \brief Platoform Abstraction Layer API for 'low level' datastore access
 
-
-## \brief Constructs a PALDatastore object
-# \param self - instance reference
-# \return PALDatastore instance
-# \author jmccrea@keesaco.com of Keesao
-#def __init__(self):
-#	pass
-
+###########################################################################
 ## open - opens a resource for write/read/edit etc.
-# \param cls - class reference
-# \param file_name - name of file to open
-# \param mode - (= 'r') mode to open file in
-# \param MIME_type - (= None) MIME type of data to access
-# \param options - (= None)
-# \param read_buff_size - (= storage_api.ReadBuffer.DEFAULT_BUFFER_SIZE) size of read buffer (max = 30MB)
-# \param retry_params - (= None)
-# \return file handle for opened file or None on failure
-# \author jmccrea@keesaco.com of Keesaco
-# \author cwike@keesaco.com of Keesaco
+## \param cls - class reference
+## \param file_name - name of file to open
+## \param mode - (= 'r') mode to open file in
+## \param MIME_type - (= None) MIME type of data to access
+## \param options - (= None)
+## \param read_buff_size - (= storage_api.ReadBuffer.DEFAULT_BUFFER_SIZE) size of read buffer (max = 30MB)
+## \param retry_params - (= None)
+## \return file handle for opened file or None on failure
+## \author jmccrea@keesaco.com of Keesaco
+## \author cwike@keesaco.com of Keesaco
+###########################################################################
 def open(	file_name,
 			mode = 'r',
 			MIME_type = None,
@@ -41,13 +36,15 @@ def open(	file_name,
 		return file_handle
 	
 	
+###########################################################################
 ## delete - deletes a resource
-# \param cls - class reference
-# \param file_name - name of resource to delete
-# \param retry_params - (= None)
-# \return boolean (true = success, false otherwise)
-# \author jmccrea@keesaco.com of Keesaco
-# \author cwike@keesaco.com of Keesaco
+## \param cls - class reference
+## \param file_name - name of resource to delete
+## \param retry_params - (= None)
+## \return boolean (true = success, false otherwise)
+## \author jmccrea@keesaco.com of Keesaco
+## \author cwike@keesaco.com of Keesaco
+###########################################################################
 def delete(	file_name,
 			retry_params = None	):
 	try:
@@ -57,14 +54,15 @@ def delete(	file_name,
 	else:
 		return True
 		
-
+###########################################################################
 ## stat - gets information about a specified resource
-# \param cls - class reference
-# \param file_name - name of resource to get information about
-# \param retry_params - (= None)
-# \return FileStat object - object containing information about specified resource, None on failure
-# \author jmccrea@keesaco.com of Keesaco
-# \author cwike@keesaco.com of Keesaco
+## \param cls - class reference
+## \param file_name - name of resource to get information about
+## \param retry_params - (= None)
+## \return FileStat object - object containing information about specified resource, None on failure
+## \author jmccrea@keesaco.com of Keesaco
+## \author cwike@keesaco.com of Keesaco
+###########################################################################
 def stat(	file_name,
 			retry_params = None	):
 	try:
@@ -76,18 +74,19 @@ def stat(	file_name,
 	else:
 		return file_stat
 	
-	
+###########################################################################
 ## list_bucket - lists the contents of a storage bucket
-# \param cls - class reference
-# \param path - path of bucket to list (string)
-# \param marker - (= None) further path, only resources following this marker will be listed
-# \param prefix - (= None) DEPRECATED (Use path prefix)
-# \param max_keys - (= None) limits the number of resources listed (None = no limit)
-# \param delimiter - (= None) used to turn on directory mode (string of one or multiple characters used as a directory seperator)
-# \param retry_params - (= None)
-# \return list of FileStat objects
-# \author jmccrea@keesaco.com of Keesaco
-# \author cwike@keesaco.com of Keesaco
+## \param cls - class reference
+## \param path - path of bucket to list (string)
+## \param marker - (= None) further path, only resources following this marker will be listed
+## \param prefix - (= None) DEPRECATED (Use path prefix)
+## \param max_keys - (= None) limits the number of resources listed (None = no limit)
+## \param delimiter - (= None) used to turn on directory mode (string of one or multiple characters used as a directory seperator)
+## \param retry_params - (= None)
+## \return list of FileStat objects
+## \author jmccrea@keesaco.com of Keesaco
+## \author cwike@keesaco.com of Keesaco
+###########################################################################
 def list_bucket(	path,
 					marker = None,
 					prefix = None, ##deprecated
