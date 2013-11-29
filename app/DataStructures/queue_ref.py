1581bc7e923ed9208fa7c950c0536d0713949468
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-11-29 17:39:14 +0000

    Added user/priority changes to queue_ref
    Also changed the names of some variables for ease of use.

--- app/DataStructures/queue_ref.py
+++ app/DataStructures/queue_ref.py
@@ -17,13 +17,13 @@ class queue_ref:
 		## The id number for the specific analysis.
 		self.analysis_id_number = analysis_id_number
 		## The user_id list for the users performing this analysis.
-		self.user_id = [user_id]
+		self.users = [user_id]
 		if priority < 0:
 			priority = 0
 		## The priority list for this item in the queue.
-		self.priority = [priority]
+		self.priorities = [priority]
 		## The priority garnered through aging by this object.
-		self.add_priority = 0
+		self.total_priority = 0
 		return self
 	
 	## \brief Ages the reference allowing for even the lowest priority references to have a chance at being processed.
@@ -31,7 +31,35 @@ class queue_ref:
 	# \return Returns True.
 	# \author swhitehouse@keesaco.com of Keesaco
 	def age_reference(self):
-		self.add_priority += sum(self.priority)
+		self.total_priority += sum(self.prioritys)
 		return True
+	
+	## \brief Adds an additional user to the queue_ref object along with their associated priority
+	# \param self - instance reference
+	# \param user_id - the new user to be added to the object
+	# \param priority - the priority associated with the new user
+	# \return Returns True on success, False if fails.
+	# \note Will fail if the user already exists in the list of users.
+	# \author swhitehouse@keesaco.com of Keesaco
+	def add_user(self, user_id, priority):
+		if user_id in self.users:
+			return False
+		self.users.append(user_id)
+		self.priorities.append(priority)
+		return True
+	
+	## \brief Removes a user from the queue_ref object
+	# \param self - instance reference
+	# \param user_id - the user to be removed from the object
+	# \return Returns True on success, False if fails.
+	# \note Will fail if the user does not exist in the list of users.
+	# \note Will also remove the priority associated with the old user.
+	# \author swhitehouse@keesaco.com of Keesaco
+	def remove_user(self, user_id):
+		if user_id in self.users:
+			self.priorities.remove(self.users.index(user_id))
+			self.users.remove(user_id)
+			return True
+		return False
 	
 
