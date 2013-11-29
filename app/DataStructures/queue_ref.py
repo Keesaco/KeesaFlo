5c132c7312b7f9f60f4085dda52d02d48792ef90
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-11-29 16:56:35 +0000

    Cleaned up doxygen comments in the queue_ref file.
    Also fixed incorrect initialisers for creation of lists.

--- app/DataStructures/queue_ref.py
+++ app/DataStructures/queue_ref.py
@@ -9,19 +9,19 @@ class queue_ref:
 	# \param self - instance reference
 	# \param analysis_id_number - the id number for the specific analysis
 	# \param user_id - the id number for the original user
-	# \param priority - (= 0) the user priority for the object.
+	# \param priority - (= 0) the user priority for the object
 	# \return Returns queue_ref object.
-	# \warning Does not check that the analysis_id_number references anything
+	# \warning Does not check that the analysis_id_number references anything.
 	# \author swhitehouse@keesaco.com of Keesaco
 	def __init__(self, analysis_id_number, user_id, priority = 0):
 		## The id number for the specific analysis.
 		self.analysis_id_number = analysis_id_number
 		## The user_id list for the users performing this analysis.
-		self.user_id[] = [user_id]
-		## The priority list for this item in the queue.
+		self.user_id = [user_id]
 		if priority < 0:
 			priority = 0
-		self.priority[] = [priority]
+		## The priority list for this item in the queue.
+		self.priority = [priority]
 		## The priority garnered through aging by this object.
 		self.add_priority = 0
 		return self
