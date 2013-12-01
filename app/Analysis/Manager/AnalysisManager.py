cedd3bd758870bed2951b1480cb3e2fb72e918d2
Jack McCrea <jmccrea@keesaco.com>
2013-12-01 01:28:11 +0000

    Improve doxygen header layout and add package documentation

--- app/Analysis/Manager/AnalysisManager.py
+++ app/Analysis/Manager/AnalysisManager.py
@@ -1,15 +1,22 @@
+###########################################################################
 ## \file app/Analysis/Manager/AnalysisManager.py
-# \brief Defines the Analysis Manager which is responsible for instantiating plugins and providing information to the instantiations.
-# \author swhitehouse@keesaco.com of Keesaco
+## \brief Defines the Analysis Manager which is responsible for instantiating plugins and providing information to the instantiations.
+## \author swhitehouse@keesaco.com of Keesaco
+###########################################################################
+## \package app.Analysis.Manager.AnalysisManager
+## \brief Provides an Analysis Manager class which is responsible for instantiating plugins and providing information to the instantiations.
+###########################################################################
 
 from PALAnalysis import *
 
 ## \brief Analysis Manager responsible for instantiating plugins and providing them with data.
 class AnalysisManager:
 
+	###########################################################################
 	## \brief Constructor for AnalysisManager.
-	# \param self - instance reference
-	# \return Returns the AnalysisManager instance.
-	# \author swhitehouse@keesaco.com of Keesaco
+	## \param self - instance reference
+	## \return Returns the AnalysisManager instance.
+	## \author swhitehouse@keesaco.com of Keesaco
+	###########################################################################
 	def __init__(	self	):
 		pass
--- app/DataStructures/plugin_ref.py
+++ app/DataStructures/plugin_ref.py
@@ -1,17 +1,26 @@
+###########################################################################
 ## \file app/DataStructures/plugin_ref.py
-# \brief Contains the plugin reference class.
-# \author swhitehouse@keesaco.com of Keesaco
+## \brief Contains the plugin reference class.
+## \author swhitehouse@keesaco.com of Keesaco
+###########################################################################
+## \package app.DataStructures.plugin_ref
+## \brief Provides a plugin referece
+## \todo The name of this package/file violates our naming convention, should be fixed
+###########################################################################
+
 
 ## \brief Plugin reference class for holding information about the location of a plugin.
 class plugin_ref:
 	
+	###########################################################################
 	## \brief Constructor for the plugin_ref object.
-	# \param self - instance reference
-	# \param path - path through which the plugin can be located in the data store
-	# \return Returns plugin_ref object on success. Returns false on fail.
-	# \note Will fail if no path is given.
-	# \warning Does not check that the path for the plugin exists.
-	# \author swhitehouse@keesaco.com of Keesaco
+	## \param self - instance reference
+	## \param path - path through which the plugin can be located in the data store
+	## \return Returns plugin_ref object on success. Returns false on fail.
+	## \note Will fail if no path is given.
+	## \warning Does not check that the path for the plugin exists.
+	## \author swhitehouse@keesaco.com of Keesaco
+	###########################################################################
 	def __init__(self, path):
 		if path == None:
 			return False
