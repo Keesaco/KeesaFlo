a46c70ae63ff3ab2dc31bc75d03ebaf5d206a322
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-12-07 13:20:56 +0000

    Created PALAnalysisManager

---
+++ app/Analysis/Manager/PALAnalysisManager.py
@@ -0,0 +1,14 @@
+###########################################################################
+## \file app/Analysis/Manager/PALAnalysisManager.py
+## \brief Defines the Platform Abstraction Layer for the Analysis Manager which is responsible for instantiating plugins.
+## \author rmurley@keesaco.com of Keesaco
+## \author swhitehouse@keesaco.com of Keessaco
+###########################################################################
+## \package app.Analysis.Manager.PALAnalysisManager
+## \brief Provides an PAL Analysis Manager class which can instantiate plugins on the google servers and provide them with information.
+###########################################################################
+
+class PALAnalysisManager:
+
+	pass
+
