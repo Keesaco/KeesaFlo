9ca70bcbb5dc95bc6aa1b9ca6b1620db40f654d0
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-12-09 19:13:51 +0000

    Merges types files and renamed AdminConfig

--- app/Analysis/Types.py
+++ app/Analysis/Types.py
@@ -1,11 +1,62 @@
 ###########################################################################
-## \file app/Analysis/Types/IntTypes.py
+## \file app/Analysis/Types.py
 ## \brief Contains all analysis types.
 ## \author swhitehouse@keesaco.com of Keesaco
 ###########################################################################
-## \package app.Analysis.Types.IntTypes
-## \brief Provides all internal type classes associated with analysis.
+## \package app.Analysis.Types
+## \brief Provides all type classes associated with analysis.
 ###########################################################################
+
+
+## \brief Data reference class for holding information about a portion of a data set.
+class DataReference:
+
+	###########################################################################
+	## \brief Constructor for the DataReference object.
+	## \param self - instance reference
+	## \param path - path through which the data can be located in the data store
+	## \param set - (= [(0, 0)]) a list of the start and end points (in bytes) of the subsets of data to be analysed
+	## \return Returns DataReference object on success. Returns false on fail.
+	## \note Will fail if no path is given.
+	## \note Will fail if first value in the tuple is greater than the second.
+	## \note Leaving set as default will include all of the available data in analysis.
+	## \warning Does not check that the path for the data exists.
+	## \author swhitehouse@keesaco.com of Keesaco
+	###########################################################################
+	def __init__(	self,
+					path,
+					set = [(0, 0)]	):
+		if path == None:
+			return False
+		for (x, y) in set:
+			if x > y:
+				return False
+		## The path at which the data can be found.
+		self.path = path
+		## The subset of data being referenced.
+		self.subset = set
+		return self
+
+
+## \brief Plugin reference class for holding information about the location of a plugin.
+class PluginReference:
+	
+	###########################################################################
+	## \brief Constructor for the PluginReference object.
+	## \param self - instance reference
+	## \param path - path through which the plugin can be located in the data store
+	## \return Returns PluginReference object on success. Returns false on fail.
+	## \note Will fail if no path is given.
+	## \warning Does not check that the path for the plugin exists.
+	## \author swhitehouse@keesaco.com of Keesaco
+	###########################################################################
+	def __init__(	self,
+					path	):
+		if path == None:
+			return False
+		## The path at which the plugin can be found.
+		self.path = path
+		return self
 
 
 ## \brief Analysis id, responsible for holding information on a specific analysis and its users
