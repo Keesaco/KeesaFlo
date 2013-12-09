f01dbb9b256d7cbc980a41db4e01d04880239fd7
Jack McCrea <jmccrea@keesaco.com>
2013-12-09 20:40:10 +0000

    Fix PAL open call in APIDatastore.add_directory

--- app/API/APIDatastore.py
+++ app/API/APIDatastore.py
@@ -177,7 +177,7 @@ def add_directory(	path,
 		return False
 	else:
 		if check_exists(get_container(path,permissions):
-			file_handle = PALDatastore(open, 'w')
+			file_handle = PALDatastore.open(path, 'w')
 			if file_handle is None:
 				return False
 			else:
