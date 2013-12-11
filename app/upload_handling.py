f9164a0c4de4250a067eae52777ef2a6d73d30b9
Purpwood <rogan@roganmurley.com>
2013-12-11 22:19:10 +0000

    Implemented very poor upload handling.

--- app/upload_handling.py
+++ app/upload_handling.py
@@ -1,24 +1,21 @@
 # Custom upload handler class.
 from django.core.files.uploadhandler import FileUploadHandler
-#import APIdatastore
+import API.APIDatastore as ds
 
 # Custom upload handler class.
 class fcsUploadHandler(FileUploadHandler):
     def __init__(self):
         FileUploadHandler.__init__(self)
-        #add_directory('/raw_data')
-        #self.path = generate_path('default/', 'raw_data', file_name) # SHOULD NOT BE NEEDED!
 
     # Called when a new file upload is starting, before upload handlers get any data.
     def new_file(self, field_name, file_name, content_type, content_length, charset):
-        #self.path = generate_path('/', 'raw_data', file_name)
-        #add_file(self.path)
+        self.path = ds.generate_path('/fc-raw-data/', 'raw_data', file_name)
         print 'New file upload starting: %s (%s) [%s]' % (file_name, content_type, content_length)
         return None
 
     # Receives a "chunk" of data from file upload.
     def receive_data_chunk(self, raw_data, start):
-        #append(self.path, raw_data)
+        ds.add_file(self.path, raw_data, 'w')
         print 'Chunk get!'
         return None
 
@@ -29,6 +26,6 @@ class fcsUploadHandler(FileUploadHandler):
         print 'File upload complete!'
         return None
 
-    # Change chunk size experiment.
+    # Change the size of chunks buffered in memory.
     def chunk_size():
         return 2**31
