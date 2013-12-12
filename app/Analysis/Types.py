5154767d65e6af8d4a162ca1ac9ac5735c632f13
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-12-12 19:30:19 +0000

    Completed functionality for the ScheduleQueue type

--- app/Analysis/Types.py
+++ app/Analysis/Types.py
@@ -25,7 +25,7 @@ class DataReference:
 	###########################################################################
 	def __init__(	self,
 					path,
-					set = [(0, 0)]	):
+					subsets = [(0, 0)]	):
 		if path == None:
 			return False
 		for (x, y) in set:
@@ -34,8 +34,7 @@ class DataReference:
 		## The path at which the data can be found.
 		self.path = path
 		## The subset of data being referenced.
-		self.subset = set
-		pass
+		self.subset = subsets
 
 
 ## \brief Plugin reference class for holding information about the location of a plugin.
@@ -56,7 +55,6 @@ class PluginReference:
 			return False
 		## The path at which the plugin can be found.
 		self.path = path
-		pass
 
 
 ## \brief Analysis id, responsible for holding information on a specific analysis and its users
@@ -85,7 +83,6 @@ class AnalysisID:
 		self.data_ref = data_ref
 		## The PluginReference object for this analysis.
 		self.plugin_ref = plugin_ref
-		pass
 	
 	###########################################################################
 	## \brief Adds an additional user to the AnalysisID object
@@ -132,24 +129,19 @@ class AnalysisID:
 
 
 ## \brief Queue reference class holding a priority and an analysis id.
-class QueueReference:
+class QueueElement:
 
 	###########################################################################
-	## \brief Constructor for the QueueReference object.
+	## \brief Constructor for the QueueElement object.
 	## \param self - instance reference
-	## \param id_number - the id number for the specific analysis
 	## \param user_id - the id number for the original user
 	## \param priority - (= 0) the user priority for the object
-	## \return Returns QueueReference object.
-	## \warning Does not check that the analysis_id_number references anything.
+	## \return Returns QueueElement object.
 	## \author swhitehouse@keesaco.com of Keesaco
 	###########################################################################
 	def __init__(	self,
-					analysis_id_number,
 					user_id,
 					priority = 1	):
-		## The id number for the specific analysis.
-		self.id_number = analysis_id_number
 		## The user_id list for the users performing this analysis.
 		self.users = [user_id]
 		if priority < 1:
@@ -158,20 +150,18 @@ class QueueReference:
 		self.priorities = [priority]
 		## The priority garnered through ageing by this object.
 		self.total_priority = 0
-		pass
 	
 	###########################################################################
-	## \brief Ages the reference allowing for low priority references to be processed.
+	## \brief Ages the element by adding to its total priority.
 	## \param self - instance reference
-	## \return Does not return anything, pass.
+	## \return Does not return anything.
 	## \author swhitehouse@keesaco.com of Keesaco
 	###########################################################################
-	def age_reference(	self	):
+	def age_element(	self	):
 		self.total_priority += sum(self.priorities)
-		pass
 	
 	###########################################################################
-	## \brief Adds an additional user to the QueueReference object along with their priority.
+	## \brief Adds an additional user to the QueueElement object along with their priority.
 	## \param self - instance reference
 	## \param user_id - the new user to be added to the object
 	## \param priority - (= 1) the priority associated with the new user
@@ -191,7 +181,7 @@ class QueueReference:
 		return True
 	
 	###########################################################################
-	## \brief Removes a user from the QueueReference object.
+	## \brief Removes a user from the QueueElement object.
 	## \param self - instance reference
 	## \param user_id - the user to be removed from the object
 	## \return Returns True on success, False if fails.
@@ -208,7 +198,7 @@ class QueueReference:
 		return False
 
 	###########################################################################
-	## \brief Checks for a user from the QueueReference object.
+	## \brief Checks for a user from the QueueElement object.
 	## \param self - instance reference
 	## \param user_id - the user to be checked for in the object
 	## \return Returns the number of occurrences of the user.
@@ -221,7 +211,7 @@ class QueueReference:
 		return self.users.count(user_id)
 
 	###########################################################################
-	## \brief Checks how many users there are in a QueueReference object.
+	## \brief Checks how many users there are in a QueueElement object.
 	## \param self - instance reference
 	## \return Returns the number users. If fails, returns False.
 	## \note The function will fail if the number of users is not equal to the number of priorities.
@@ -235,10 +225,10 @@ class QueueReference:
 		return False
 
 
-## Schedule Queue class holding the priority queue of queue references for the schedule.
+## Schedule Queue class holding the dictionary of queue elements for the schedule.
 class ScheduleQueue:
 
-	from Analysis.Types import QueueReference
+	from app.Analysis.Types import QueueElement
 	
 	###########################################################################
 	## \brief Constructor for the ScheduleQueue object.
@@ -247,53 +237,89 @@ class ScheduleQueue:
 	## \author swhitehouse@keesaco.com of Keesaco
 	###########################################################################
 	def __init__(	self	):
-		## A list which will form the queue itself.
-		self.references = []
-		pass
+		## A dictionary which will form the queue itself.
+		self.elements = {}
 
 	###########################################################################
-	## \brief Adds a user to a queue reference if it exists, and makes a new one if it doesn't.
+	## \brief Ages each element in the queue.
 	## \param self - instance reference
-	## \return Returns True on success, False if fails.
-	## \note Will fail if the user already exists in the specific reference.
+	## \return Does not return anything.
 	## \author swhitehouse@keesaco.com of Keesaco
 	###########################################################################
-	def add_user_reference(	self,
-							analysis_id_number,
-							user_id,
-							priority = 1	):
-		for ref in self.references:
-			if ref.id_number == analysis_id_number:
-				return ref.add_user(user_id, priority)
-		self.references.append(QueueReference(analysis_id_number, user_id, priority))
+	def age_queue(	self	):
+		for analysis_id in self.elements:
+			self.elements[analysis_id].age_element()
+
+	###########################################################################
+	## \brief Checks how many elements there are in the queue.
+	## \param self - instance reference
+	## \return Does not return anything.
+	## \author swhitehouse@keesaco.com of Keesaco
+	###########################################################################
+	def check_number_elements(	self	):
+		return len(self.elements)
+
+	###########################################################################
+	## \brief Adds a user to a queue element if it exists, and makes a new one if it doesn't.
+	## \param self - instance reference
+	## \param analysis_id - the unique identifier for an analysis used as a key in the dictionary
+	## \param user_id - the user to be added to the queue element
+	## \param priority - the priority of the user to be added
+	## \return Returns True on success, False if fails.
+	## \note Will fail if the user already exists in the specific element.
+	## \author swhitehouse@keesaco.com of Keesaco
+	###########################################################################
+	def add_user_to_element(	self,
+								analysis_id,
+								user_id,
+								priority = 1	):
+		if analysis_id in self.elements:
+			return self.elements[analysis_id].add_user(user_id, priority)
+		self.elements[analysis_id] = QueueElement(user_id, priority)
 		return True
 
 	###########################################################################
-	## \brief Removes a user from a queue reference if it exists, removes the reference if they were the last user.
+	## \brief Removes a user from a queue element if it exists, removes the element if they were the last user.
 	## \param self - instance reference
+	## \param analysis_id - the unique identifier for an analysis used as a key in the dictionary
+	## \param user_id - the user to be removed from the queue element
 	## \return Returns True on success, False if fails.
-	## \note Will fail if the user does not exist in the reference, or if the reference itself doesn't exist.
+	## \note Will fail if the user does not exist in the element, or if the element itself doesn't exist.
 	## \author swhitehouse@keesaco.com of Keesaco
 	###########################################################################
-	def remove_user_reference(	self,
-								analysis_id_number,
-								user_id	):
-		for ref in self.references:
-			if ref.id_number == analysis_id_number:
-				if ref.remove_user(user_id):
-					if ref.check_number_users() == 0:
-						self.references.remove(ref)
+	def remove_user_from_element(	self,
+									analysis_id,
+									user_id	):
+		if analysis_id in self.elements:
+			if self.elements[analysis_id].remove_user(user_id):
+					if self.elements[analysis_id].check_number_users() == 0:
+						del self.elements[analysis_id]
 					return True
 		return False
 
 	###########################################################################
-	## \brief Ages each reference in the queue.
+	## \brief Provides the caller with the element in the list with the highest priority.
 	## \param self - instance reference
-	## \return Does not return anything, pass.
+	## \return Returns the analysis_id of the highest priority element, or False if the queue is empty.
 	## \author swhitehouse@keesaco.com of Keesaco
 	###########################################################################
-	def age_queue(	self	):
-		for ref in self.references:
-			ref.age_reference()
-		pass		
+	def next_element(	self	):
+		if self.check_number_elements() == 0:
+			return False
+		analysis_id = max(iter(self.elements), key=(lambda key: self.elements[key].total_priority))
+		del self.elements[analysis_id]
+		return analysis_id
+	
+	###########################################################################
+	## \brief Checks to see if an element is in the queue.
+	## \param self - instance reference
+	## \param analysis_id - the unique identifier for an analysis used as a key in the dictionary
+	## \return Returns True if the element is in the queue, False if not.
+	## \author swhitehouse@keesaco.com of Keesaco
+	###########################################################################
+	def check_element(	self,
+						analysis_id	):
+		if analysis_id in self.elements:
+			return True
+		return False
 
