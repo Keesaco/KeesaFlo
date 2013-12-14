534540ef8eb95aca9cf95c1ece4caea69452793e
Sam Whitehouse <samwise.the.tired@hotmail.co.uk>
2013-12-14 17:53:05 +0000

    Updated the API

--- app/API/APIAnalysis.py
+++ app/API/APIAnalysis.py
@@ -8,64 +8,53 @@
 ## \brief Provides an API for accessing the analysis plugin management.
 ###########################################################################
 
+from app.Analysis.Manager import AnalysisManager
 
-## \brief API for analysis managing requests to the Analysis API.
-class APIAnalysis:
+###########################################################################
+## \brief Is called to request that a new analysis be created or added to.
+## \param user_id - the id of the user
+## \param priority - a value determining the priority of the user
+## \param data_location - the DataLocation object to be used in analysis
+## \param plugin_location - the PluginLocation object to be used in analysis
+## \return On fail, return false. On success, returns the analysis_id_number for the specific analysis request.
+## \note user_id is used primarily to make sure that other users cannot mess with the original user's analysis.
+## \warning This function does not check that the user has permissions to perform the specified analysis. Nor does it check whether the analysis has already been performed.
+## \author swhitehouse@keesaco.com of Keesaco
+## \author rmurley@keesaco.com of Keesaco
+###########################################################################
+def add_user_analysis(	user_id,
+						priority,
+						data_location,
+						plugin_location	):
+	return AnalysisManager.subscribe_user_analysis(user_id, priority, data_location, plugin_location)
+	
 
-	###########################################################################
-	## \brief Constructor for APIAnalysis.
-	## \param self - instance reference
-	## \return Returns the APIAnalysis instance.
-	## \author swhitehouse@keesaco.com of Keesaco
-	###########################################################################
-	def __init__(	self	):
-		pass
-
-	###########################################################################
-	## \brief Is called to request that a new analysis using a specific plugin and data set be added to the scheduler.
-	## \param data_ref - reference to the data to be analysis
-	## \param plugin_ref - reference to the plugin to be used for analysis
-	## \param user_id - the id of the user starting the analysis
-	## \param priority - a value determining the priority of the specific analysis
-	## \return On fail, return false. On success, returns the analysis_id_number for the specific analysis request.
-	## \note user_id is used primarily to make sure that other users cannot mess with the original user's analysis.
-	## \warning This function does not check that the user has permissions to perform the specified analysis. Nor does it check whether the analysis has already been performed.
-	## \author swhitehouse@keesaco.com of Keesaco
-	## \author rmurley@keesaco.com of Keesaco
-	###########################################################################
-	def add_analysis(	data_ref,
-						plugin_ref,
-						user_id,
-						priority	):
-		pass
-
-	###########################################################################
-	## \brief Requests that the specific analysis instantiation is ended prematurely.
-	## \param analysis_id - the id of the analysis that needs to be ended
-	## \param user_id - the id of the user ending the analysis
-	## \return If analysis termination is successful, return True. Else False.
-	## \note May not actually cancel analysis, unless all users related to that analysis request for it to be cancelled.
-	## \note Will fail if the user_id does not match stored values in the analysis_id.
-	## \note Will fail if the analysis_id does not exist.
-	## \author swhitehouse@keesaco.com of Keesaco
-	## \author rmurley@keesaco.com of Keesaco
-	###########################################################################
-	def remove_analysis(	analysis_id,
+###########################################################################
+## \brief Requests that a user is removed from an analysis request.
+## \param analysis_id - the id of the analysis to be removed from
+## \param user_id - the id of the user to be removed
+## \return Returns True on success, False if fails.
+## \note Will fail if the user_id does not match stored values in the analysis_id.
+## \note Will fail if the analysis_id does not exist.
+## \author swhitehouse@keesaco.com of Keesaco
+## \author rmurley@keesaco.com of Keesaco
+###########################################################################
+def remove_user_analysis(	analysis_id,
 							user_id	):
-		pass
+	return AnalysisManager.unsubscribe_user_analysis(analysis_id, user_id)
 
-	###########################################################################
-	## \brief Requests that the current state of the analysis is checked.
-	## \param analysis_id - the id of the analysis that is being checked
-	## \param user_id - the id of the user checking the analysis
-	## \return Returns information on the current state of the analysis. If failed, return False.
-	## \note Will either return the analysis' position in the schedule, or the expected time remaining on the actual analysis.
-	## \note Will fail if the user_id does not match stored values in the analysis_id.
-	## \note Will fail if the analysis_id does not exist.
-	## \author swhitehouse@keesaco.com of Keesaco
-	## \author rmurley@keesaco.com of Keesaco
-	###########################################################################
-	def check_analysis(	analysis_id,
-						user_id	):
-		pass
+###########################################################################
+## \brief Requests that the current state of the analysis is checked.
+## \param analysis_id - the id of the analysis that is being checked
+## \param user_id - the id of the user checking the analysis
+## \return Returns information on the current state of the analysis. If failed, return False.
+## \note Will either return the analysis' position in the schedule, or the expected time remaining on the actual analysis.
+## \note Will fail if the user_id does not match stored values in the analysis_id.
+## \note Will fail if the analysis_id does not exist.
+## \author swhitehouse@keesaco.com of Keesaco
+## \author rmurley@keesaco.com of Keesaco
+###########################################################################
+def check_analysis(	analysis_id,
+					user_id	):
+	return AnalysisManager.check_analysis(analysis_id, user_id)
 
