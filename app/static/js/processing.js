541ccc9d9cc1cf28942b5ae872cef345fac89634
Jack McCrea <jmccrea@keesaco.com>
2014-02-07 00:43:14 +0000

    WIP - add pagelet load methods to CS framework

--- app/static/js/processing.js
+++ app/static/js/processing.js
@@ -1,8 +1,34 @@
 /*!************************************************************************
-#** \file app/static/js/processing.js
+** \file app/static/js/processing.js
 ** \brief JavaScript library for the client-side processing layer
 ** \author jmccrea@keesaco.com of Keesaco
 ***************************************************************************
 ** \package app.static.js.processing
 ** \brief Provides methods for modifying data from the data layer for use by the front-end UI
 **************************************************************************/
+
+/*!************************************************************************
+** \fn ksfProc()
+** \brief ksfProc constructor used for namespace
+** \author jmccrea@keesaco.com of Keesaco
+** \note This constructor currently (intentionally) does not have any effect
+***************************************************************************/
+function ksfProc()
+{
+}
+
+/*!************************************************************************
+** \fn ksfProc.copyPageletInto(URI, targetID)
+** \brief gets a pagelet and displays its contents in the target container
+** \param URI - [string] pagelet URI
+** \param targetID - [string] (Should include '#' prefix) ID of the element to show the pagelet in
+** \author jmccrea@keesaco.com of Keesaco
+***************************************************************************/
+function ksfProc.copyPageletInto(URI, targetID)
+{
+	///get data and do stuff
+	ksfData.fetch( 	URI,
+					function(response) {
+						ksfData.pageletBody(response)
+					} );
+}
--- app/static/js/request.js
+++ app/static/js/request.js
@@ -5,4 +5,28 @@
 ***************************************************************************
 ** \package app.static.js.request
 ** \brief Provides a 'low level' request layer for making HTTP requests on behalf of the data layer
+** \note this package assumes that jQuery has already been downloaded by the client
 **************************************************************************/
+
+
+/*!************************************************************************
+** \fn ksfReq()
+** \brief ksfReq constructor used for namespace
+** \author jmccrea@keesaco.com of Keesaco
+** \note This constructor currently (intentionally) does not have any effect
+***************************************************************************/
+function ksfReq()
+{
+}
+
+/*!************************************************************************
+** \fn ksfReq.fetch(URI, callback)
+** \brief makes an HTTP request for a given URI and calls the given callback with the reseponse
+** \param URI - [string] URI to request
+** \param callback	- [function] called with response
+** \author jmccrea@keesaco.com of Keesaco
+***************************************************************************/
+function ksfReq.fetch(URI, callback)
+{
+	$.get(URI, callback);
+}
