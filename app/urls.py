c267f977729628988ed3efefb4d55c15ff498792
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2014-02-09 15:08:54 +0000

    Commented page and pagelet generator

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,13 @@
+###########################################################################
+## \file app/ulrs.py
+## \brief Defines the allowed urls of the webapp
+## \author mrudelle@keesaco.com of Keesaco
+## \author rmurley@keesaco.com of Keesaco
+###########################################################################
+
 from django.conf.urls.defaults import *
 from django.contrib import admin
-import views #TODO may need to be moved, to be decided later
+import views 
 import dbindexer
 
 handler500 = 'djangotoolbox.errorviews.server_error'
@@ -11,15 +18,23 @@ admin.autodiscover()
 # search for dbindexes.py in all INSTALLED_APPS and load them
 dbindexer.autodiscover()
 
+
 urlpatterns = patterns('',
+
+    # Used by Django to start the web app
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
-    #('^$', 'django.views.generic.simple.direct_to_template', {'template': 'index.html'}),
+
+    # Various pages that can be requested
     url(r'^$', 'views.index', name='index'),
     url(r'^about/$', 'views.about', name='about'),
     url(r'^faq/$', 'views.faq', name='faq'),
     url(r'^app/$', 'views.app', name='app'),
+    url(r'^settings/$', 'views.settings', name='settings'),
+
+    # Various pagelet of the app page that can be requested 
     url(r'^app/file=(?P<file>.+)$', 'views.file_page', name='pagelet_file'),
     url(r'^app/file_list/$', 'views.file_list', name='pagelet_list'),
-    url(r'^settings/$', 'views.settings', name='settings'),
+
+    # Used for administration purpose
     ('^admin/', include(admin.site.urls))
 )
