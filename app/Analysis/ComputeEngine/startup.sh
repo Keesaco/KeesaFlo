3957fc363a54a7382d7ffd8aa3961f1ab6459832
Purpwood <rogan@roganmurley.com>
2014-02-09 21:59:15 +0000

    Ready for Compute Engine

---
+++ app/Analysis/ComputeEngine/startup.sh
@@ -0,0 +1,11 @@
+###########################################################################
+## \file app/Analysis/Manager/startup.sh
+## \brief Runs the FCS analysis on new instances.
+## \author swhitehouse@keesaco.com of Keesaco
+###########################################################################
+
+#!/bin/bash
+## Obtains the file location from the metadata of the instance request.
+FILE_LOCATION=$(curl http://metadata/computeMetadata/v1/instance/attributes/file_location -H "X-Google-Metadata-Request: True")
+## Runs the analysis on the file location.
+python visualise.py $FILE_LOCATION
