6dc9403efe04ccb3ffcb3195b710e2ac52f4f6ae
Hazel Doughty <hdoughty93@gmail.com>
2014-02-14 12:52:00 +0000

    App panel now slides with toolbar on mobile
    TODO: fix window resizing issues

--- app/static/js/offcanvas.js
+++ app/static/js/offcanvas.js
@@ -1,9 +1,14 @@
 $(function() {
     $('.toggletools').click(function() {
         var $marginLefty = $('#sidebar2');
+        var $marginPanel = $('.apppanel');
         $marginLefty.animate({
             marginLeft: parseInt($marginLefty.css('marginLeft'), 10) == 0 ?
                 -$marginLefty.outerWidth() : 0
-        });  
+        });
+        $marginPanel.animate({
+            marginLeft: parseInt($marginPanel.css('marginLeft'), 10) == 0 ?
+                $marginLefty.outerWidth() : 0
+        });
     });
 });
