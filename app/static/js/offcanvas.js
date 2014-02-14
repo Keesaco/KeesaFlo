3af68623ccdbf3dc1513c3bc8fcbb54cca1a3616
Hazel Doughty <hdoughty93@gmail.com>
2014-02-14 16:47:07 +0000

    Fixed browser resizing issues on toolbar
    Also fixed issue with app panel not filling all space from previous
    commit

--- app/static/js/offcanvas.js
+++ app/static/js/offcanvas.js
@@ -1,7 +1,7 @@
 $(function() {
+    var $marginLefty = $('#sidebar2');
+    var $marginPanel = $('.apppanel');
     $('.toggletools').click(function() {
-        var $marginLefty = $('#sidebar2');
-        var $marginPanel = $('.apppanel');
         $marginLefty.animate({
             marginLeft: parseInt($marginLefty.css('marginLeft'), 10) == 0 ?
                 -$marginLefty.outerWidth() : 0
@@ -10,5 +10,23 @@ $(function() {
             marginLeft: parseInt($marginPanel.css('marginLeft'), 10) == 0 ?
                 $marginLefty.outerWidth() : 0
         });
+    });
+    $(window).resize(function() {
+        var $browserWidth = $(window).width();
+        if($browserWidth > 767 && parseInt($marginLefty.css('marginLeft'), 10) == 0) {
+            $marginPanel.css({
+                marginLeft: '0'
+            });
+        }
+        else if($browserWidth > 767) {
+            $marginLefty.css({
+                marginLeft: '0'
+            });
+        }
+        else {
+            $marginLefty.css({
+                marginLeft: -$marginLefty.outerWidth()
+            });
+        }
     });
 });
