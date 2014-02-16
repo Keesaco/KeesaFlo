940998810a1dc634ac248e622d8f0ca13a5f1926
Hazel Doughty <hdoughty93@gmail.com>
2014-02-16 16:11:05 +0000

    This fixes #10

--- app/static/js/togglelink.js
+++ app/static/js/togglelink.js
@@ -4,7 +4,12 @@ $(function() {
         $marginRighty.animate({
             marginRight: parseInt($marginRighty.css('marginRight'), 10) == 0 ?
                 -$marginRighty.outerWidth() : 0
-        });  
+        });
+        var $browserWidth = $(window).width();
+        if($browserWidth <= 767 && parseInt($marginRighty.css('marginRight'), 10) < 0) {
+            $('#dropdownmenu').toggleClass('collapse');
+            $('#dropdownmenu').toggleClass('in');
+        }
     });
     $(window).resize(function() {
         if(parseInt($marginRighty.css('marginRight'), 10) < 0) {
