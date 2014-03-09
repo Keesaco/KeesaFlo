97defaaf90fae67394f6f5f63e0b00e39310fdb7
Purpwood <rogan@roganmurley.com>
2014-03-09 13:53:41 +0000

    Added to doxygen to queue API and PAL.

--- app/API/APIQueue.py
+++ app/API/APIQueue.py
@@ -1,8 +1,27 @@
-# Analysis queue API.
+###########################################################################
+## \file app/API/APIQueue.py
+## \brief Defines the task queue API for queuing tasks. Built upon PALQueue for cross-platform support.
+## \author rmurley@keesaco.com of Keesaco
+###########################################################################
+## \package app.API.APIQueue
+## \brief Provides an API for accessing the job queue.
+## \note Depends on PALQueue
+## \sa app.API.PALQueue
+###########################################################################
+
 import API.PALQueue as queue
 
+###########################################################################
+## \brief Adds a task to visualise fcs data
+## \param filename - filename of fcs data to visualise
+## \author rmurley@keesaco.com of Keesaco
+###########################################################################
 def visualise(	filename ):
 	queue.add_task('vis;' + filename)
 
+###########################################################################
+## \brief Adds a task to kill a virtual machine instance.
+## \author rmurley@keesaco.com of Keesaco
+###########################################################################
 def kill():
     queue.add_task('kill;' + filename)
--- app/API/PALQueue.py
+++ app/API/PALQueue.py
@@ -1,4 +1,13 @@
-# Analsis queue PAL.
+###########################################################################
+## \file app/API/PALQueue.py
+## \brief Contains the PALQueue package: Platform Abstraction Layer for task queue access.
+## \author rmurley@keesaco.com of Keesaco
+###########################################################################
+## \package app.API.PALQueue
+## \brief Contains abstraction layer functions for Google taskqueue - returns results in a platform independent form
+## \brief Provides an API for accessing the job queue.
+###########################################################################
+
 from google.appengine.api import taskqueue
 
 def add_task( task ):
