3046980a446e7a09cb112499cc82f3d259ca7f84
Hazel Doughty <hdoughty93@gmail.com>
2014-03-12 14:31:54 +0000

    Added polygon gating and updated APIAnalysis

--- compute_engine/API/APIAnalysis.py
+++ compute_engine/API/APIAnalysis.py
@@ -61,6 +61,33 @@ def rect_gate(  name, top_left_x, top_left_y, bottom_right_x, bottom_right_y):
 	subprocess.call(["Rscript", "gate.r", name, name + 'gate.png', top_left_x, top_left_y, bottom_right_x, bottom_right_y])
 
 ###########################################################################
+## \brief Saves a visualisation of a gated fcs file
+## \param name - name of fcs file to gate
+## \pararm mean_x - x coordinate of the mean of the gate
+## \pararm mean_y - y coordinate of the mean of the gate
+## \pararm a_x - x coordinate of the point further from the mean
+## \pararm a_y - y coordinate of the point further from the mean
+## \pararm b_x - x coordinate of the point closest to the mean
+## \pararm b_y - y coordinate of the point closest to the mean
+## \return returns True if successful else False
+## \note all parameters should be strings
+## \author hdoughty@keesaco.com of Keesaco
+###########################################################################
+def oval_gate(name, mean_x, mean_y, a_x, a_y, b_x, b_y):
+	subprocess.call(["Rscript", "ovalgate.r", name, name + 'gate.png', mean_x, mean_y, a_x, a_y, b_x, b_y])
+
+###########################################################################
+## \brief Saves a visualisation of a gated fcs file
+## \param name - name of fcs file to gate
+## \pararm points - string of all the points which define the polygon gate
+## \return returns True if successful else False
+## \note all parameters should be strings
+## \author hdoughty@keesaco.com of Keesaco
+###########################################################################
+def poly_gate(name, points):
+	subprocess.call(["Rscript", "polygate.r", name, name + 'gate.png', points])
+
+###########################################################################
 ## \brief Saves a visualisation image from local disk to Datastore
 ## \param name - name of image file to save
 ## \param permissions - user attempting to perform access
