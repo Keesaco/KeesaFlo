cb4e9f4183a52384154da3e42b068ec9a1f2d7bb
Hazel Doughty <hdoughty93@gmail.com>
2014-03-16 18:29:35 +0000

    Added gating code to visualise.py

--- compute_engine/visualise.py
+++ compute_engine/visualise.py
@@ -2,6 +2,7 @@
 ## \file compute_engine/visualise.py
 ## \brief Visualises flow cytometry data using Google Cloud Storage and Bioconductor. Depends on AnalysisAPI.
 ## \author rmurley@keesaco.com of Keesaco
+## \author hdoughty@keesaco.com of Keesaco
 ###########################################################################
 ## \package compute_engine.visualise
 ## \brief Visualises flow cytometry data using Google Cloud Storage and Bioconductor. Should be called by the Compute Engine startup sheel script. Depends on AnalysisAPI.
@@ -37,6 +38,56 @@ while alive:
 		## Clean up.
 		os.remove(name)
 		os.remove(name + '.png')
+	elif (commands[0] == 'gate_rec'):
+		name = commands[1]
+		points = commands[2]
+		gate_name = commands[3]
+		coords = points.split()
+        ##Loads raw fcs data from cloud storage
+        Ana.load_fcs(name)
+        ##Creates visualisation of gate and text file of gate info
+        Ana.rect_gate(name, gate_name, coords[0], coords[1], coords[2], coords[3])
+        ## Save visualisation to cloud storage.
+        Ana.save_vis(gate_name + '.png')
+        ## Saves info about gate to cloud storage
+        Ana.save_info(gate_name + '.txt')
+        ## Clean up.
+        os.remove(name)
+        os.remove(gate_name + '.txt')
+        os.remove(gate_name + '.png')
+    elif (commands[0] == 'gate_cir'):
+        name = commands[1]
+        points = commands[2]
+        gate_name = commands[3]
+        coords = points.split()
+        ##Loads raw fcs data from cloud storage
+        Ana.load_fcs(name)
+        ##Creates visualisation of gate and text file of gate info
+        Ana.oval_gate(name, gate_name, coords[0], coords[1], coords[2], coords[3], coords[4], coords[5])
+        ## Save visualisation to cloud storage.
+        Ana.save_vis(gate_name + '.png')
+        ## Saves info about gate to cloud storage
+        Ana.save_info(gate_name + '.txt')
+        ## Clean up.
+        os.remove(name)
+        os.remove(gate_name + '.txt')
+        os.remove(gate_name + '.png')
+    elif (commands[0] == 'gate_poly'):
+        name = commands[1]
+        points = commands[2]
+        gate_name = commands[3]
+        ##Loads raw fcs data from cloud storage
+        Ana.load_fcs(name)
+        ##Creates visualisation of gate and text file of gate info
+        Ana.poly_gate(name, gate_name, points)
+        ## Save visualisation to cloud storage.
+        Ana.save_vis(gate_name + '.png')
+        ## Saves info about gate to cloud storage
+        Ana.save_info(gate_name + '.txt')
+        ## Clean up.
+        os.remove(name)
+        os.remove(gate_name + '.txt')
+        os.remove(gate_name + '.png')
 	# Delete any processed tasks from queue.
 	if task_id is not None:
 		Queue.delete('jobs', task_id)
