0d7a94a72e915597d4deb46afa5bb621f9fbc77b
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2014-03-24 01:09:57 +0000

    toogleLink.js cleaned

--- app/static/js/togglelink.js
+++ app/static/js/togglelink.js
@@ -1,29 +1,44 @@
+function ksfToogleLink() {
+}
+
+FILE_SIDE_BAR = '#sidebar';
+
+ksfToogleLink.fileSelector;
+
 $(function(){
-	var $marginRighty = $('#sidebar');
-	$('.togglefiles').click(function(){
-		$marginRighty.animate({
-			marginRight: parseInt($marginRighty.css('marginRight'), 10) == 0 ?
-			-$marginRighty.outerWidth() : 0
-		});
-		var $browserWidth = $(window).width();
-		if($browserWidth <= 767 && parseInt($marginRighty.css('marginRight'), 10) < 0) {
-			$('#dropdownmenu').toggleClass('collapse');
-			$('#dropdownmenu').toggleClass('in');
-		}
-	});
-	$(window).resize(function() {
-		if(parseInt($marginRighty.css('marginRight'), 10) < 0) {
-			$marginRighty.css({
-				marginRight: -$marginRighty.outerWidth()
-			});
-		}
-	});
+    ksfToogleLink.fileSelector = $(FILE_SIDE_BAR);
+    ksfToogleLink.addToogleFilesListener();
+    $(window).resize(function() {
+        if(parseInt(ksfToogleLink.fileSelector.css('marginRight'), 10) < 0) {
+            ksfToogleLink.fileSelector.css({
+                marginRight: -ksfToogleLink.fileSelector.outerWidth()
+            });
+        }
+    });
 });
 
-function fileSelector()
+ksfToogleLink.fileSelector = function()
 {
-	$('#sidebar').animate({
-		marginRight: 0
-	});
+    ksfToogleLink.fileSelector.animate({
+        marginRight: 0
+    });
+}
+
+ksfToogleLink.toogleFileSelector = function()
+{
+    ksfToogleLink.fileSelector.animate({
+        marginRight: parseInt(ksfToogleLink.fileSelector.css('marginRight'), 10) == 0 ?
+        -ksfToogleLink.fileSelector.outerWidth() : 0
+    });
+    var browserWidth = $(window).width();
+    if(browserWidth <= 767 && parseInt(ksfToogleLink.fileSelector.css('marginRight'), 10) < 0) {
+        $('#dropdownmenu').toggleClass('collapse');
+        $('#dropdownmenu').toggleClass('in');
+    }
+}
+
+ksfToogleLink.addToogleFilesListener = function() 
+{
+    $('.togglefiles').click(ksfToogleLink.toogleFileSelector);
 }
 
