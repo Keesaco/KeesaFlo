6f0788b6c473212237802db920e6a0c305c6b22b
samwisethetired <samwise.the.tired@hotmail.co.uk>
2014-03-24 17:54:20 +0000

    Fixed the help tour for mobile and web devices, and handle switching between the two mediums.

--- app/static/js/helptour.js
+++ app/static/js/helptour.js
@@ -1,163 +1,252 @@
-var tour = new Tour
+// The bootstrap-tour object which handles the tour.
+var keesafloTour = new Tour
 ({
-	debug: true
+	name:		"keesaflo-tour",
+	steps:		[
+					{
+						title:		"Welcome to KeesaFlo!",
+						content:	"KeesaFlo is a collaborative flow-cytometry analysis application.",
+						backdrop:	true,
+						orphan:		true,
+						next:		2,
+						prev:		-1
+					},
+					{
+						title:		"Welcome to KeesaFlo!",
+						content:	"KeesaFlo is a collaborative flow-cytometry analysis application.",
+						backdrop:	true,
+						orphan:		true,
+						next:		3,
+						prev:		-1
+					},
+					{
+						element: 	".tour-step.account",
+						placement: 	"left",
+						title: 		"Account Options",
+						content: 	"Click here for settings, to logout, or to go through this tour again.",
+						next:		4,
+						prev:		0
+					},
+					{
+						element:	".tour-step.account",
+						placement:	"bottom",
+						title: 		"Account Options",
+						content: 	"Click here for settings, to logout, or to go through this tour again.",
+						next:		5,
+						prev:		1,
+						onShow:		function(tour)
+									{
+										setDropDown(true);
+									},
+						onPrev:		function()
+									{
+										setDropDown(false);
+									}
+					},
+					{
+						element: 	".tour-step.files",
+						placement: 	"left",
+						title: 		"Files",
+						content: 	"This button shows the file panel, allowing you access to all of your previously uploaded flow cytometry files.",
+						next:		6,
+						prev:		2
+					},
+					{
+						element:	".tour-step.files",
+						placement:	"bottom",
+						title: 		"Files",
+						content: 	"This button shows the file panel, allowing you access to all of your previously uploaded flow cytometry files.",
+						next:		7,
+						prev:		3,
+						onShow:		function(tour)
+									{
+										setDropDown(true);
+									}
+					},
+					{
+						element: 	".tour-step.upload",
+						placement: 	"top",
+						title: 		"Upload",
+						content: 	"Click this button to upload your new flow cytometry (.fcs) data.",
+						next:		8,
+						prev:		4,
+						onShow:		function()
+									{
+										setFileBar(true);
+									},
+						onNext:		function()
+									{
+										setFileBar(false);
+									},
+						onPrev:		function()
+									{
+										setFileBar(false);
+									},
+					},
+					{
+						element: 	".tour-step.upload",
+						placement: 	"top",
+						title: 		"Upload",
+						content: 	"Click this button to upload your new flow cytometry (.fcs) data.",
+						next:		9,
+						prev:		5,
+						onShow:		function()
+									{
+										setDropDown(true);
+										setFileBar(true);
+									},
+						onNext:		function()
+									{
+										setFileBar(false);
+										setDropDown(false);
+									},
+						onPrev:		function()
+									{
+										setFileBar(false);
+									}
+					},
+					{
+						element:	".tour-step.tools",
+						placement:	"right",
+						title:		"Tools",
+						content:	"Use these tools to gate your flow cytometry files.",
+						next:		10,
+						prev:		6
+					},
+					{
+						element:	".tour-step.tools",
+						placement:	"right",
+						title:		"Tools",
+						content:	"Use these tools to gate your flow cytometry files.",
+						next:		11,
+						prev:		7,
+						onShow:		function()
+									{
+										setToolBar(true);
+									},
+						onNext:		function()
+									{
+										setToolBar(false);
+									},
+						onPrev:		function()
+									{
+										setToolBar(false);
+									}
+					},
+					{
+						title:		"The End",
+						content:	"We hope you enjoy using Keesaflo. If you have any questions, check the FAQ in the bottom left corner.",
+						orphan:		true,
+						next:		-1,
+						prev:		8
+					},
+					{
+						title:		"The End",
+						content:	"We hope you enjoy using Keesaflo. If you have any questions, check the FAQ in the bottom left corner.",
+						orphan:		true,
+						next:		-1,
+						prev: 		9
+					}
+				],
+	debug:		true
 });
 
-tour.addSteps
-([
-	{
-		title:		"Welcome to KeesaFlo!",
-		content:	"KeesaFlo is a collaborative flow-cytometry analysis application.",
-		backdrop:	true,
-		orphan:		true,
-		next:		1,
-		prev:		-1,
-	},
-	{
-		element: 	".tour-step.account",
-		placement: 	"left",
-		title: 		"Account Options",
-		content: 	"Click here for settings, to logout, or to go through this tour again.",
-		next:		3,
-		prev:		0
-	},
-	{
-		element:	".tour-step.account",
-		placement:	"bottom",
-		title: 		"Account Options",
-		content: 	"Click here for settings, to logout, or to go through this tour again.",
-		next:		4,
-		prev:		0,
-		onShow:		function()
-					{
-						$('#dropdownmenu').collapse('show');
-					},
-		onPrev:		function()
-					{
-						$('#dropdownmenu').collapse('hide');
-					}
-	},
-	{
-		element: 	".tour-step.files",
-		placement: 	"left",
-		title: 		"Files",
-		content: 	"This button shows the file panel, allowing you access to all of your previously uploaded flow cytometry files.",
-		next:		5,
-		prev:		1
-	},
-	{
-		element:	".tour-step.files",
-		placement:	"bottom",
-		title: 		"Files",
-		content: 	"This button shows the file panel, allowing you access to all of your previously uploaded flow cytometry files.",
-		next:		6,
-		prev:		2
-	},
-	{
-		element: 	".tour-step.upload",
-		placement: 	"top",
-		title: 		"Upload",
-		content: 	"Click this button to upload your new flow cytometry (.fcs) data.",
-		next:		7,
-		prev:		3,
-		onShow:		function()
-					{
-						$('#sidebar').animate({
-							marginRight: 0
-						});
-					},
-		onHide:		function()
-					{
-						$('#sidebar').animate({
-							marginRight: -$('#sidebar').outerWidth()
-						});
-					}
-	},
-	{
-		element: 	".tour-step.upload",
-		placement: 	"top",
-		title: 		"Upload",
-		content: 	"Click this button to upload your new flow cytometry (.fcs) data.",
-		next:		8,
-		prev:		4,
-		onShow:		function()
-					{
-						$('#dropdownmenu').collapse('show');
-						$('#sidebar').animate({
-							marginRight: 0
-						});
-					},
-		onNext:		function()
-					{
-						$('#sidebar').animate({
-							marginRight: -$('#sidebar').outerWidth()
-						});
-						$('#dropdownmenu').collapse('hide');
-					},
-		onPrev:		function()
-					{
-						$('#sidebar').animate({
-							marginRight: -$('#sidebar').outerWidth()
-						});
-					}
-	},
-	{
-		element:	".tour-step.tools",
-		placement:	"right",
-		title:		"Tools",
-		content:	"Use these tools to gate your flow cytometry files.",
-		next:		9,
-		prev:		5
-	},
-	{
-		element:	".tour-step.tools",
-		placement:	"right",
-		title:		"Tools",
-		content:	"Use these tools to gate your flow cytometry files.",
-		next:		9,
-		prev:		6,
-		onShow:		function()
-					{
-						$('#sidebar2').animate({
-							marginLeft: 0
-						});
-						$('.apppanel').animate({
-							marginLeft: $('#sidebar2').outerWidth()
-						});
-					},
-		onHide:		function()
-					{
-						$('#sidebar2').animate({
-							marginLeft: -$('#sidebar2').outerWidth()
-						});
-						$('.apppanel').animate({
-							marginLeft: 0
-						});
-					}
-	},
-	{
-		title:		"The End",
-		content:	"We hope you enjoy using Keesaflo. If you have any questions, check the FAQ in the bottom left corner.",
-		orphan:		true,
-		next:		-1,
-		prev:		7
-	}
-]);
-
-// Initialize the tour
-tour.init();
-
-// Start the tour
-tour.start();
-
-function helpForceStart()
+// Shows the tool bar on true, hides on false.
+function setToolBar(is_out)
 {
-	if (tour.ended()){
-		tour.restart();
+	if(is_out)
+	{
+		$('#sidebar2').animate({
+			marginLeft: 0
+		});
+		$('.apppanel').animate({
+			marginLeft: $('#sidebar2').outerWidth()
+		});
+	}
+	else{
+		$('#sidebar2').animate({
+			marginLeft: -$('#sidebar2').outerWidth()
+		});
+		$('.apppanel').animate({
+			marginLeft: 0
+		});
+	}
+}
+
+// Shows the drop down on true, hides on false.
+function setDropDown(is_out)
+{
+	if(is_out)
+	{
+		$('#dropdownmenu').collapse('show');
+	}
+	else{
+		$('#dropdownmenu').collapse('hide');
+	}
+}
+
+// Shows the file bar on true, hides on false.
+function setFileBar(is_out)
+{
+	if(is_out)
+	{
+		$('#sidebar').animate({
+			marginRight: 0
+		});
+	}
+	else{
+		$('#sidebar').animate({
+			marginRight: -$('#sidebar').outerWidth()
+		});
+	}
+}
+
+// Function to begin the help tour.
+function helpTourBegin(force)
+{
+	if(force)
+	{
+		keesafloTour.restart();
 	}
 	else
 	{
-		tour.start(true);
+		keesafloTour.start();
+	}
+	if($(window).width() <= 767)
+	{
+		keesafloTour.setCurrentStep(1);
+		keesafloTour.goTo(1);
 	}
 }
+
+// Updated the help tour when the window is resized.
+$(function(){
+	$(window).resize(function() {
+		var $browserWidth = $(window).width();
+		currentStep = keesafloTour.getCurrentStep();
+		if($browserWidth <= 767)
+		{
+			if((currentStep % 2) == 0)
+			{
+				currentStep += 1;
+				keesafloTour.setCurrentStep(currentStep);
+				keesafloTour.goTo(currentStep);
+			}
+		}
+		else{
+			if((currentStep % 2) == 1)
+			{
+				currentStep -= 1;
+				keesafloTour.setCurrentStep(currentStep);
+				keesafloTour.goTo(currentStep);
+			}
+		}
+	});
+});
+
+// Initialize the tour
+keesafloTour.init();
+
+// Runs the help tour.
+helpTourBegin(false);
 
