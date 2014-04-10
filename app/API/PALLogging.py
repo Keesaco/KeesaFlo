938734e20e92b2e0c03236a1d420fd0e28c84bff
Jack McCrea <jmccrea@keesaco.com>
2014-04-10 01:45:04 +0100

    Implement APILogging

--- app/API/PALLogging.py
+++ app/API/PALLogging.py
@@ -1,5 +1,5 @@
 ###########################################################################
-## \file 	app/API/PALDatastore.py
+## \file 	app/API/PALLogging.py
 ## \brief 	Containts the PALLogging package: Platform Abstraction Layer
 ##			for application event logging
 ## \author 	jmccrea@keesaco.com of Keesaco
@@ -16,46 +16,46 @@
 import logging
 
 ###########################################################################
-## \brief Creates a log at the debug level
-## \param String msg - the message to log
-## \return None
+## \brief 	Creates a log at the debug level
+## \param 	String msg - the message to log
+## \return	None
 ## \author jmccrea@keesaco.com of Keesaco
 ###########################################################################
 def log_debug(msg):
 	logging.debug(msg)
 
 ###########################################################################
-## \brief Creates a log at the info level
-## \param String msg - the message to log
-## \return None
+## \brief 	Creates a log at the info level
+## \param 	String msg - the message to log
+## \return	None
 ## \author jmccrea@keesaco.com of Keesaco
 ###########################################################################
 def log_info(msg):
 	logging.info(msg)
 
 ###########################################################################
-## \brief Creates a log at the warning level
-## \param String msg - the message to log
-## \return None
-## \author jmccrea@keesaco.com of Keesaco
+## \brief 	Creates a log at the warning level
+## \param 	String msg - the message to log
+## \return 	None
+## \author 	jmccrea@keesaco.com of Keesaco
 ###########################################################################
 def log_warning(msg):
 	logging.warning(msg)
 
 ###########################################################################
-## \brief Creates a log at the error level
-## \param String msg - the message to log
-## \return None
-## \author jmccrea@keesaco.com of Keesaco
+## \brief 	Creates a log at the error level
+## \param 	String msg - the message to log
+## \return 	None
+## \author 	jmccrea@keesaco.com of Keesaco
 ###########################################################################
 def log_error(msg):
 	logging.error(msg)
 
 ###########################################################################
-## \brief Creates a log at the critical level
-## \param String msg - the message to log
-## \return None
-## \author jmccrea@keesaco.com of Keesaco
+## \brief 	Creates a log at the critical level
+## \param 	String msg - the message to log
+## \return 	None
+## \author 	jmccrea@keesaco.com of Keesaco
 ###########################################################################
 def log_critical(msg):
 	logging.critical(msg)
