84836fce6af0522eb6f4d35555f7524cd11750f9
Matthieu Rudelle <matthieu.rudelle@epfl.ch>
2014-04-12 13:08:01 +0200

    Simplifies gating tool implementation
    As requested the dictionary for the tools was replaced by a elif bloc

--- app/tools.py
+++ app/tools.py
@@ -8,69 +8,51 @@ import API.APIQueue as queue
 from django.core.urlresolvers import reverse
 import logging
 
-###########################################################################
-## \brief Is called when a rectangular gating is requested.
-## \param params - Paramesters of this gating, string of the form: topLeftx,topLefty,bottomRightx,bottomRighty,newFilename
-## \return a dictionary with the status of the gating, a short message and the link to the newly created graph
-## \author mrudelle@keesaco.com of Keesaco
-###########################################################################
-def rect_gating(paramList):
-	if len(paramList) == 5 :
-		#Reoder the point to take the topLeft and bottomRight points of the square 
-		if paramList[0] > paramList[2]:
-			tempcoor = paramList[0]
-			paramList[0] = paramList[2]
-			paramList[2] = tempcoor
-		if paramList[1] > paramList[3]:
-			tempcoor = paramList[1]
-			paramList[1] = paramList[3]
-			paramList[3] = tempcoor
+def simple_gating(paramList, name):
+	gateName = paramList.pop(0);
 
-		gatingRequest =" ".join(paramList[0:4])
+	if (gateName == "rectangular_gating") :
+		if len(paramList) == 5 :
+			#Reoder the point to take the topLeft and bottomRight points of the square 
+			if paramList[0] > paramList[2]:
+				tempcoor = paramList[0]
+				paramList[0] = paramList[2]
+				paramList[2] = tempcoor
+			if paramList[1] > paramList[3]:
+				tempcoor = paramList[1]
+				paramList[1] = paramList[3]
+				paramList[3] = tempcoor
 
-		newName = paramList[-1] + "-rectGate";
-		queue.gate_rectangle(paramList[-1], gatingRequest, newName, "1", "FSC-A", "PE-A");
-		return generate_gating_feedback("success", "the rectangular gating was performed correctly", reverse('get_graph', args=[newName]))
+			gatingRequest =" ".join(paramList[0:4])
 
-	else:
-		return generate_gating_feedback("fail", "notcorrect " + params + " length:" + str(len(paramList)) + " is not equal to 4", None)
+			newName = paramList[-1] + "-rectGate";
+			queue.gate_rectangle(paramList[-1], gatingRequest, newName, "1", "FSC-A", "PE-A");
+			return generate_gating_feedback("success", "the rectangular gating was performed correctly", reverse('get_graph', args=[newName]))
+		else:
+			return generate_gating_feedback("fail", "notcorrect " + params + " length:" + str(len(paramList)) + " is not equal to 4", None)
 
+	elif (gateName == "polygon_gating") :
+		if len(paramList)%2 == 1 :
+			gatingRequest = " ".join(paramList[0:-1])        
 
-###########################################################################
-## \brief Is called when a polygonal gating is requested.
-## \param params - Paramesters of this gating, string of the form: x1,y1,x2,y2,...xn,yn,newFilename
-## \return a dictionary with the status of the gating, a short message and the link to the newly created graph
-## \author mrudelle@keesaco.com of Keesaco
-###########################################################################
-def poly_gating(paramList):
-	if len(paramList)%2 == 1 :
-		gatingRequest = " ".join(paramList[0:-1])        
+			newName = paramList[-1] + "-polyGate";
+			queue.gate_polygon(paramList[-1], gatingRequest, newName, "0", "FSC-A", "PE-A");
+			return generate_gating_feedback("success", "the polygonal gating was performed correctly", reverse('get_graph', args=[newName]))
+		else:
+			return generate_gating_feedback("fail", "notcorrect " + params + " #pointCoordinates:" + str(len(paramList))-1 + " is not pair", None)
 
-		newName = paramList[-1] + "-polyGate";
-		queue.gate_polygon(paramList[-1], gatingRequest, newName, "0", "FSC-A", "PE-A");
-		return generate_gating_feedback("success", "the polygonal gating was performed correctly", reverse('get_graph', args=[newName]))
+	elif (gateName == "oval_gating") :
+		if len(paramList) == 7 :
+			gatingRequest = " ".join(paramList[0:-1])        
 
-	else:
-		return generate_gating_feedback("fail", "notcorrect " + params + " #pointCoordinates:" + str(len(paramList))-1 + " is not pair", None)
-
-
-###########################################################################
-## \brief Is called when an oval gating is requested.
-## \param params - Paramesters of this gating, string of the form: meanx,meany,point1x,point1y,point2x,point2y,newFilename
-## \return a dictionary with the status of the gating, a short message and the link to the newly created graph
-## \author mrudelle@keesaco.com of Keesaco
-###########################################################################
-def oval_gating(paramList):
-	if len(paramList) == 7 :
-		gatingRequest = " ".join(paramList[0:-1])        
-
-		newName = paramList[-1] + "-ovalGate";
-		queue.gate_circle(paramList[-1], gatingRequest, newName, "0", "FSC-A", "PE-A");
-		return generate_gating_feedback("success", "the oval gating was performed correctly", reverse('get_graph', args=[newName]))
-
-	else:
-		return generate_gating_feedback("fail", "notcorrect " + params + " #pointCoordinates:" + str(len(paramList)) + " is not even", None)
-
+			newName = paramList[-1] + "-ovalGate";
+			queue.gate_circle(paramList[-1], gatingRequest, newName, "0", "FSC-A", "PE-A");
+			return generate_gating_feedback("success", "the oval gating was performed correctly", reverse('get_graph', args=[newName]))
+		else:
+			return generate_gating_feedback("fail", "notcorrect " + params + " #pointCoordinates:" + str(len(paramList)) + " is not even", None)
+		
+	else :
+		return generate_gating_feedback("fail", "The gate "+gateName+" is not known", None)
 
 ###########################################################################
 ## \brief Is called when the requested tool is not in the dictionary of known tools
@@ -102,7 +84,5 @@ def generate_gating_feedback(status, message, newgraphurl):
 	}
 
 AVAILABLE_TOOLS = {
-	'rectangular_gating' : rect_gating,
-	'polygon_gating' : poly_gating,
-	'oval_gating' : oval_gating
+	'gating' : simple_gating
 }
