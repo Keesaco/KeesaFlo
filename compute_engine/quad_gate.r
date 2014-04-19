80cf706103aced975e04f12550496fa6aa3f69ee
Hazel Doughty <hdoughty93@gmail.com>
2014-04-19 14:33:53 +0100

    Added quadrant gating

---
+++ compute_engine/quad_gate.r
@@ -0,0 +1,42 @@
+###########################################################################
+## \file compute_engine/quad_gate.r
+## \brief Applies a filter fitting a bivariate normal distribution to gate
+## \author hdoughty@keesaco.com of Keesaco
+###########################################################################
+source('rfunctions.r')
+imports()
+
+## Parse arguments.
+args <- commandArgs(trailingOnly = TRUE)
+fcs_name <- args[1]
+x_coord <- as.integer(args[2])
+y_coord <- as.integer(args[3])
+quad1_name <- args[4]
+quad2_name <- args[5]
+quad3_name <- args[6]
+quad4_name <- args[7]
+x_axis <- args[8]
+y_axis <- args[9]
+
+## Read fcs data.
+if(file.exists(fcs_name) == FALSE)
+	quit("no", 1)
+x <- read.FCS(fcs_name, transformation = FALSE)
+
+## Creates gate
+boundaries <- setNames(as.list(c(x_coord, y_coord)), c(x_axis, y_axis))
+qg <- quadGate(boundaries)
+fres <- filter(x, qg)
+quadrants <- split(x, fres)
+
+## Creates new .fcs file and image for each quadrant
+quadrant_names <- c(quad1_name, quad2_name, quad3_name, quad4_name)
+for(i in 1:4)
+{
+	## Save gate as fcs file
+	write.FCS(quadrants[[i]], quadrant_names[i])
+	## Plots the gate
+	image_name <- paste(quadrant_names[i], ".png", sep="")
+	plotGraph(image_name, quadrants[[i]], x_axis, y_axis)
+}
+quit("no", 0)
