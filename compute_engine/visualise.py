aaf276c8b461087ac89c64480a153da177e21c46
Hazel Doughty <hdoughty93@gmail.com>
2014-04-24 18:51:09 +0100

    Re-factored visualise.py

--- compute_engine/visualise.py
+++ compute_engine/visualise.py
@@ -11,167 +11,130 @@ import os, time, sys
 import API.APIAnalysis as Ana
 import API.APIQueue as Queue
 
-# Check task queue until 'kill' command is received.
-alive = True
-while alive:
-	# Lease a command.
-	try:
-		task = Queue.lease('jobs', 30)
-	except Exception, e:
-		print e
-	# If there are tasks in queue extract id and commands, else restart loop and check queue again.
-	if task is not None:
-		task_id = task[0]
-		commands = task[1]
-	else:
-		time.sleep(0.9)
-		continue
-	# If command is to kill instance, stop looping and exit python script.
-	if (commands[0] == 'kill'):
-		alive = False
-	# If command is to visualise, visualise.
-	elif (commands[0] == 'vis'):
-		# Get name.
-		name = commands[1]
-		## Load raw fcs data from cloud storage.
-		Ana.load_fcs(name)
-		## Create visualisation of raw fcs data.
-		exitcode = Ana.visualise(name)
-		if(exitcode == 0):
-			## Save visualisation to cloud storage.
-			Ana.save_vis(name + '.png')
-			## Saves info about fcs file to cloud storage
-			Ana.save_info(name + 'info.txt')
-			## Clean up.
-			os.remove(name)
-			os.remove(name + '.png')
-			os.remove(name + 'info.txt')
-	elif (commands[0] == 'gate_rec' or commands[0] == 'gate_cir' or commands[0] == 'gate_poly' or 
-		commands[0] == 'gate_bool'):
-		name = commands[1]
-		points = commands[2]
-		gate_name = commands[3]
-		reverse = commands[4]
-		x_axis = commands[5]
-		y_axis = commands[6]
-		coords = points.split()
-		##Loads raw fcs data from cloud storage
-		Ana.load_fcs(name)
-		##Creates visualisation of gate and text file of gate info
-		if(commands[0] == 'gate_rec'):
-			exitcode = Ana.rect_gate(name, gate_name, coords[0], coords[1], coords[2], 
-				coords[3], reverse, x_axis, y_axis)
-		elif(commands[0] == 'gate_cir'):
-			exitcode = Ana.oval_gate(name, gate_name, coords[0], coords[1], coords[2], 
-				coords[3], coords[4], coords[5], reverse, x_axis, y_axis)
-		elif(commands[0] == 'gate_poly'):
-			exitcode = Ana.poly_gate(name, gate_name, points, reverse, x_axis, y_axis)
-		elif(commands[0] == 'gate_bool'):
-			exitcode = Ana.bool_gate(name, gate_name, commands[7], commands[8], points, reverse, commands[9], 
-				commands[10], commands[11], x_axis, y_axis, commands[12], commands[13])
-		if(exitcode == 0):
-			## Save visualisation to cloud storage.
-			Ana.save_vis(gate_name + '.png')
-			## Converts info file from .txt to .html
-			f = open(gate_name + '.txt', 'r')
-			info = f.readline()
-			f.close()
-			stats = info.split()
-			f2 = open(gate_name + '.html', 'w')
-			f2.write('<link href="{static "css/app.css" %}" rel ="stylesheet"')
-			f2.write('<p>Number of cells in gate: <span class="gating_num">' + stats[0] + '</span></p>')
-			f2.write('<p>Number of cells in total: <span class="gating_num">' + stats[1] + '</span></p>')
-			f2.write('<p>Percentage ratio: <span class="gating_num">' + str(float(stats[2])*100) + '%</span></p>')
-			f2.close()
-			## Saves info about gate to cloud storage
-			Ana.save_info(gate_name + '.html')
-			## Saves gate as fcs file
-			Ana.save_fcs(gate_name)
-			## Clean up.
-			os.remove(gate_name + '.txt')
-			os.remove(gate_name + '.png')
-			os.remove(gate_name)
-			os.remove(name)
-	elif (commands[0] == 'change_axis'):
-		name = commands[1]
-		x_axis = commands[2]
-		y_axis = commands[3]
-		##Loads raw fcs data from cloud storage
-		Ana.load_fcs(name)
-		##Creates a visualisation of the graph with different axis
-		exitcode = Ana.change_axis(name, x_axis, y_axis)
-		if(exitcode == 0):
-			##Saves visualisation to cloud storage
-			Ana.save_vis(name + x_axis + y_axis + '.png')
-			## Clean up
-			os.remove(name)
-			os.remove(name + x_axis + y_axis + '.png')
-	elif (commands[0] == 'dot_plot' or commands[0] == 'contour_plot'):
-		name = commands[1]
-		x_axis = commands[2]
-		y_axis = commands[3]
-		plot_name = commands[4]
-		##Load raw fcs data from cloud storage
-		Ana.load_fcs(name)
-		##Create visualisation of graph with different plot type
-		if(commands[0] == 'dot_plot'):
-			exitcode = Ana.dot_plot(name, plot_name, x_axis, y_axis)
-		elif(commands[0] == 'contour_plot'):
-			exitcode = Ana.contour_plot(name, plot_name, x_axis, y_axis)
-		if(exitcode == 0):
-			## Saves visualisation to cloud storage
-			Ana.save_vis(plot_name)
-			## Clean up
-			os.remove(name)
-			os.remove(plot_name)
-	elif (commands[0] == 'gate_norm'):
+def gate_clean_up(gate_name):
+	os.remove(gate_name + '.png')
+	os.remove(gate_name + '.txt')
+	os.remove(gate_name)
+
+def save_gate(gate_name):
+	## Saves visualisation of gate to cloud storage
+	Ana.save_vis(gate_name + '.png')
+	## Saves info about gate to cloud storage
+	Ana.save_info(gate_name + '.txt')
+	## Saves gate as fcs file
+	Ana.save_fcs(gate_name)
+
+def shape_gating(commands):
+	name = commands[1]
+	points = commands[2]
+	gate_name = commands[3]
+	reverse = commands[4]
+	x_axis = commands[5]
+	y_axis = commands[6]
+	coords = points.split()
+	##Loads raw fcs data from cloud storage
+	Ana.load_fcs(name)
+	##Creates visualisation of gate and text file of gate info
+	if(commands[0] == 'gate_rec'):
+		exitcode = Ana.rect_gate(name, gate_name, coords[0], coords[1], coords[2], 
+			coords[3], reverse, x_axis, y_axis)
+	elif(commands[0] == 'gate_cir'):
+		exitcode = Ana.oval_gate(name, gate_name, coords[0], coords[1], coords[2], 
+			coords[3], coords[4], coords[5], reverse, x_axis, y_axis)
+	elif(commands[0] == 'gate_poly'):
+		exitcode = Ana.poly_gate(name, gate_name, points, reverse, x_axis, y_axis)
+	elif(commands[0] == 'gate_bool'):
+		exitcode = Ana.bool_gate(name, gate_name, commands[7], commands[8], points, reverse, commands[9], 
+			commands[10], commands[11], x_axis, y_axis, commands[12], commands[13])
+	if(exitcode == 0):
+		## Save visualisation to cloud storage.
+		Ana.save_vis(gate_name + '.png')
+		## Converts info file from .txt to .html
+		f = open(gate_name + '.txt', 'r')
+		info = f.readline()
+		f.close()
+		stats = info.split()
+		f2 = open(gate_name + '.html', 'w')
+		f2.write('<link href="{static "css/app.css" %}" rel ="stylesheet"')
+		f2.write('<p>Number of cells in gate: <span class="gating_num">' + stats[0] + '</span></p>')
+		f2.write('<p>Number of cells in total: <span class="gating_num">' + stats[1] + '</span></p>')
+		f2.write('<p>Percentage ratio: <span class="gating_num">' + str(float(stats[2])*100) + '%</span></p>')
+		f2.close()
+		## Saves info about gate to cloud storage
+		Ana.save_info(gate_name + '.html')
+		## Saves gate as fcs file
+		Ana.save_fcs(gate_name)
+		## Clean up.
+		gate_clean_up(gate_name)
+		os.remove(name)
+	return True
+
+def axis_change(commands):
+	name = commands[1]
+	x_axis = commands[2]
+	y_axis = commands[3]
+	##Loads raw fcs data from cloud storage
+	Ana.load_fcs(name)
+	##Creates a visualisation of the graph with different axis
+	exitcode = Ana.change_axis(name, x_axis, y_axis)
+	if(exitcode == 0):
+		##Saves visualisation to cloud storage
+		Ana.save_vis(name + x_axis + y_axis + '.png')
+		## Clean up
+		os.remove(name)
+		os.remove(name + x_axis + y_axis + '.png')
+	return True
+
+def visualise(commands):
+	# Get name.
+	name = commands[1]
+	## Load raw fcs data from cloud storage.
+	Ana.load_fcs(name)
+	## Create visualisation of raw fcs data.
+	exitcode = Ana.visualise(name)
+	if(exitcode == 0):
+		## Save visualisation to cloud storage.
+		Ana.save_vis(name + '.png')
+		## Saves info about fcs file to cloud storage
+		Ana.save_info(name + 'info.txt')
+		## Clean up.
+		os.remove(name)
+		os.remove(name + '.png')
+		os.remove(name + 'info.txt')
+	return True
+
+def change_plot(commands):
+	name = commands[1]
+	plot_name = commands[4]
+	##Load raw fcs data from cloud storage
+	Ana.load_fcs(name)
+	##Create visualisation of graph with different plot type
+	if(commands[0] == 'dot_plot'):
+		exitcode = Ana.dot_plot(name, plot_name, commands[2], commands[3])
+	elif(commands[0] == 'contour_plot'):
+		exitcode = Ana.contour_plot(name, plot_name, commands[2], commands[3])
+	if(exitcode == 0):
+		## Saves visualisation to cloud storage
+		Ana.save_vis(plot_name)
+		## Clean up
+		os.remove(name)
+		os.remove(plot_name)
+	return True
+
+def data_gate(commands):
+	if(commands[0] == 'gate_norm'):
 		name = commands[1]
 		gate_name = commands[2]
-		reverse = commands[3]
-		x_axis = commands[4]
-		y_axis = commands[5]
-		scale_factor = commands[6]
 		##Loads raw fcs data from cloud storage
 		Ana.load_fcs(name)
 		##Creates visualisation of gate and text file of gate info
-		exitcode = Ana.norm_gate(name, gate_name, reverse, x_axis, y_axis, scale_factor)
+		exitcode = Ana.norm_gate(name, gate_name, commands[3], commands[4], commands[5], commands[6])
 		if(exitcode == 0):
-			## Saves visualisation of gate to cloud storage
-			Ana.save_vis(gate_name + '.png')
-			## Saves info about gate to cloud storage
-			Ana.save_info(gate_name + '.txt')
-			## Saves gate as fcs file
-			Ana.save_fcs(gate_name)
+			save_gate(gate_name)
 			## Clean up.
-			os.remove(gate_name + '.txt')
-			os.remove(gate_name + '.png')
-			os.remove(gate_name)
+			gate_clean_up(gate_name)
 			os.remove(name)
-	elif (commands[0] == 'gate_quad'):
-		name = commands[1]
-		x_coord = commands[2]
-		y_coord = commands[3]
-		x_axis = commands[8]
-		y_axis = commands[9]
-		##Loads raw fcs data from cloud storage
-		Ana.load_fcs(name)
-		## Creates visualisation and .fcs file of gate
-		exitcode = Ana.quad_gate(name, x_coord, y_coord, commands[4], commands[5], commands[6], commands[7], x_axis, y_axis)
-		if(exitcode == 0):
-			for i in range(4, 8):
-				current_gate_name = commands[i]
-				## Saves visualisation of gate to cloud storage
-				Ana.save_vis(current_gate_name + '.png')
-				## Saves info about gate to cloud storage
-				Ana.save_info(current_gate_name + '.txt')
-				## Saves gate as fcs file
-				Ana.save_fcs(current_gate_name)
-				## Clean up
-				os.remove(current_gate_name + '.png')
-				os.remove(current_gate_name + '.txt')
-				os.remove(current_gate_name)
-			os.remove(name)
-	elif (commands[0] == 'gate_kmeans'):
+	elif(commands[0] == 'gate_kmeans'):
 		name = commands[1]
 		cluster_names = commands[2]
 		number_clusters = commands[3]
@@ -187,16 +150,62 @@ while alive:
 			for i in range(0, number):
 				current_gate_name = names[i]
 				## Saves visualisation of gate to cloud storage
-				Ana.save_vis(current_gate_name + '.png')
-				## Saves info about gate to cloud storage
-				Ana.save_info(current_gate_name + '.txt')
-				## Saves gate as fcs file
-				Ana.save_fcs(current_gate_name)
+				save_gate(current_gate_name)
 				## Clean up
-				os.remove(current_gate_name + '.png')
-				os.remove(current_gate_name + '.txt')
-				os.remove(current_gate_name)
+				gate_clean_up(current_gate_name)
 			os.remove(name)
+	return True
+
+def quadrant_gate(commands):
+	name = commands[1]
+	##Loads raw fcs data from cloud storage
+	Ana.load_fcs(name)
+	## Creates visualisation and .fcs file of gate
+	exitcode = Ana.quad_gate(name, commands[2], commands[3], commands[4], commands[5], commands[6], commands[7], commands[8], commands[9])
+	if(exitcode == 0):
+		for i in range(4, 8):
+			current_gate_name = commands[i]
+			save_gate(current_gate_name)
+			## Clean up
+			gate_clean_up(current_gate_name)
+		os.remove(name)
+	return True
+
+def kill(commands):
+	return False
+
+AVAILABLE_TASKS = {
+	'kill'			: kill,
+	'vis'			: visualise,
+	'gate_rec'		: shape_gating,
+	'gate_cir'		: shape_gating,
+	'gate_poly'		: shape_gating,
+	'gate_bool'		: shape_gating,
+	'change_axis'	: axis_change,
+	'dot_plot'		: change_plot,
+	'contour_plot'	: change_plot,
+	'gate_quad'		: quadrant_gate,
+	'gate_norm'		: data_gate,
+	'gate_kmeans'	: data_gate
+}
+
+# Check task queue until 'kill' command is received.
+alive = True
+while alive:
+	# Lease a command.
+	try:
+		task = Queue.lease('jobs', 30)
+	except Exception, e:
+		print e
+	# If there are tasks in queue extract id and commands, else restart loop and check queue again.
+	if task is not None:
+		task_id = task[0]
+		commands = task[1]
+	else:
+		time.sleep(0.9)
+		continue
+	functionToCall = AVAILABLE_TASKS[commands[0]]
+	alive = functionToCall(commands)
 	# Delete any processed tasks from queue.
 	if task_id is not None:
 		Queue.delete('jobs', task_id)
