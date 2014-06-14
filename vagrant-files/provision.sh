3e3e8ce8f4feb2b7e290d4b9c3f8d768bf2c1335
Jack McCrea <contact@jpm.im>
2014-06-14 23:27:52 +0100

    Use dos2unix rather than recode

--- vagrant-files/provision.sh
+++ vagrant-files/provision.sh
@@ -1,13 +1,13 @@
-wget -O devserver.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.6.zip -quiet
+wget -O devserver.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.6.zip
 apt-get update
 apt-get install -y zip
 apt-get install -y unzip
 apt-get install -y python-dev
-apt-get install -y recode
+apt-get install -y dos2unix
 unzip -qq devserver.zip
 apt-get install -y python-setuptools
 apt-get install -y python-pip
 pip install pycrypto
 cp /vagrant/vagrant-files/startserver.sh /home/vagrant/start.sh
 chmod 755 /home/vagrant/start.sh
-recode ibmpc..lat1 start.sh
+dos2unix start.sh
