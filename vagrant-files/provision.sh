d83bc4c88cae87a91864658964a47ea842f995cf
Jack McCrea <contact@jpm.im>
2014-06-14 22:59:49 +0100

    Recode line endings of startserver.sh for systems which checkout with Windoze line endings

--- vagrant-files/provision.sh
+++ vagrant-files/provision.sh
@@ -3,9 +3,11 @@ apt-get update
 apt-get install -y zip
 apt-get install -y unzip
 apt-get install -y python-dev
+apt-get install -y recode
 unzip -qq devserver.zip
 apt-get install -y python-setuptools
 apt-get install -y python-pip
 pip install pycrypto
 cp /vagrant/vagrant-files/startserver.sh /home/vagrant/start.sh
-chmod 777 /home/vagrant/start.sh
+chmod 744 /home/vagrant/start.sh
+recode ibmpc..lat start.sh
